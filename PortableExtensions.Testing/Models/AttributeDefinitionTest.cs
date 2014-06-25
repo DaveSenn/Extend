7306ad57529b9a58124b41a1807e003a0a43d6be
Dave Senn <dave.jon.senn@gmail.com>
2014-06-25 11:57:28 +0200

    Add method Type.GetAttributeDefinitions, model AttributeDefinition interface IAttributeDefinition, and unit tests new features

---
+++ PortableExtensions.Testing/Models/AttributeDefinitionTest.cs
@@ -0,0 +1,43 @@
+﻿#region Using
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public class AttributeDefinitionTest
+    {
+        [Test]
+        public void AttributesTestCase()
+        {
+            var target = new AttributeDefinition<DisplayAttribute>();
+            var expected = new List<DisplayAttribute>();
+            target.Attributes = expected;
+
+            Assert.AreSame( expected, target.Attributes );
+        }
+
+        [Test]
+        public void CtorTestCase()
+        {
+            var target = new AttributeDefinition<DisplayAttribute>();
+            Assert.IsNotNull( target.Attributes );
+            Assert.AreEqual( 0, target.Attributes.Count() );
+        }
+
+        [Test]
+        public void PropertyTestCase()
+        {
+            var target = new AttributeDefinition<DisplayAttribute>();
+            var expected = typeof ( DisplayAttribute ).GetProperties().First();
+            target.Property = expected;
+
+            Assert.AreSame( expected, target.Property );
+        }
+    }
+}
--- PortableExtensions/Properties/AssemblyInfo.cs
+++ PortableExtensions/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 // General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
 
 [assembly: AssemblyTitle( "PortableExtensions" )]
-[assembly: AssemblyDescription("Portable class library containing extension methods")]
+[assembly: AssemblyDescription( "Portable class library containing extension methods" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "PortableExtensions" )]
@@ -23,5 +23,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
