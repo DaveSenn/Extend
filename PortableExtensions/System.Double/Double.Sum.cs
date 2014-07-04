0e0e1072f9d9d19f7e7af661b8511f32d4372962
Dave Senn <dave.jon.senn@gmail.com>
2014-07-04 14:00:00 +0200

    Add Sum method for Int32, Int64, Double, Decimal

---
+++ PortableExtensions/System.Double/Double.Sum.cs
@@ -0,0 +1,91 @@
+﻿#region Using
+
+using System;
+using System.Linq;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="int" />.
+    /// </summary>
+    public static partial class DoubleEx
+    {
+        /// <summary>
+        ///     Computes the sum of a sequence of the given values.
+        /// </summary>
+        /// <exception cref="OverflowException">The sum is larger than <see cref="Double.MaxValue" /></exception>
+        /// <exception cref="ArgumentNullException">values can not be null.</exception>
+        /// <param name="value">The first value.</param>
+        /// <param name="values">The other values.</param>
+        /// <returns>Returns the sum of the values.</returns>
+        public static Double Sum( this Double value, params Double[] values )
+        {
+            values.ThrowIfNull( () => values );
+
+            var list = values.ToList();
+            list.Add( value );
+            return list.Sum();
+        }
+
+        /// <summary>
+        ///     Computes the sum of a sequence of the given values.
+        /// </summary>
+        /// <exception cref="OverflowException">The sum is larger than <see cref="Double.MaxValue" /></exception>
+        /// <exception cref="ArgumentNullException">values can not be null.</exception>
+        /// <param name="value">The first value.</param>
+        /// <param name="values">The other values.</param>
+        /// <returns>Returns the sum of the values.</returns>
+        public static Double? Sum( this Double? value, params Double?[] values )
+        {
+            values.ThrowIfNull( () => values );
+
+            var list = values.ToList();
+            list.Add( value );
+            return list.Sum();
+        }
+
+        /// <summary>
+        ///     Computes the sum of the sequence of <see cref="Double" /> values that are obtained
+        ///     by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <exception cref="OverflowException">The sum is larger than <see cref="Double.MaxValue" /></exception>
+        /// <exception cref="ArgumentNullException">selector can not be null.</exception>
+        /// <typeparam name="TSource">The type of the source values.</typeparam>
+        /// <param name="value">The first value.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <param name="values">The other values.</param>
+        /// <returns>Returns the sum of the projected values.</returns>
+        public static Double Sum<TSource>( this TSource value, Func<TSource, Double> selector, params TSource[] values )
+        {
+            selector.ThrowIfNull( () => selector );
+            values.ThrowIfNull( () => values );
+
+            var list = values.ToList();
+            list.Add( value );
+            return list.Sum( selector );
+        }
+
+        /// <summary>
+        ///     Computes the sum of the sequence of nullable <see cref="Double" /> values that are obtained
+        ///     by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <exception cref="OverflowException">The sum is larger than <see cref="Double.MaxValue" /></exception>
+        /// <exception cref="ArgumentNullException">selector can not be null.</exception>
+        /// <typeparam name="TSource">The type of the source values.</typeparam>
+        /// <param name="value">The first value.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <param name="values">The other values.</param>
+        /// <returns>Returns the sum of the projected values.</returns>
+        public static Double? Sum<TSource>( this TSource value, Func<TSource, Double?> selector, params TSource[] values )
+        {
+            selector.ThrowIfNull( () => selector );
+            values.ThrowIfNull( () => values );
+
+            var list = values.ToList();
+            list.Add( value );
+            return list.Sum( selector );
+        }
+    }
+}
