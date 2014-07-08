fdc250bdfc755502c17b8a9841276f435a2d69a7
Dave Senn <dave.jon.senn@gmail.com>
2014-07-08 21:09:24 +0200

    Add new extension methods for System.Action

---
+++ PortableExtensions/System.Action/Action.ExecuteIfFalse.cs
@@ -0,0 +1,134 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="Action" />.
+    /// </summary>
+    public static partial class ActionEx
+    {
+        /// <summary>
+        ///     Executes the specified action if the given Boolean values are false,
+        ///     otherwise it executes the specified true action, if one is specified.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">False action can not be null, if all values are false.</exception>
+        /// <param name="trueAction">The action to execute if any of the given values is true.</param>
+        /// <param name="falseAction">The action to execute if the given values are false.</param>
+        /// <param name="values">The Boolean values to check.</param>
+        public static void ExecuteIfFalse(this Action falseAction, Action trueAction = null, params Boolean[] values)
+        {
+            if (values.NotAny(x => x))
+            {
+                falseAction.ThrowIfNull(() => trueAction);
+                falseAction();
+            }
+            else if (trueAction != null)
+                trueAction();
+        }
+
+        /// <summary>
+        ///     Executes the specified action if the given Boolean values are false,
+        ///     otherwise it executes the specified true action, if one is specified.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">False action can not be null, if all values are false.</exception>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="trueAction">The action to execute if any of the given values is true.</param>
+        /// <param name="falseAction">The action to execute if the given values are false.</param>
+        /// <param name="parameter">The parameter to pass to the action with gets executed.</param>
+        /// <param name="values">The Boolean values to check.</param>
+        public static void ExecuteIfFalse<T>(this Action<T> falseAction, T parameter, Action<T> trueAction = null,
+            params Boolean[] values)
+        {
+            if (values.NotAny(x => x))
+            {
+                falseAction.ThrowIfNull(() => trueAction);
+                falseAction(parameter);
+            }
+            else if (trueAction != null)
+                trueAction(parameter);
+        }
+
+        /// <summary>
+        ///     Executes the specified action if the given Boolean values are false,
+        ///     otherwise it executes the specified true action, if one is specified.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">False action can not be null, if all values are false.</exception>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="trueAction">The action to execute if any of the given values is true.</param>
+        /// <param name="falseAction">The action to execute if the given values are false.</param>
+        /// <param name="parameter1">The first parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter2">The second parameter to pass to the action with gets executed.</param>
+        /// <param name="values">The Boolean values to check.</param>
+        public static void ExecuteIfFalse<T1, T2>(this Action<T1, T2> falseAction, T1 parameter1, T2 parameter2,
+            Action<T1, T2> trueAction = null, params Boolean[] values)
+        {
+            if (values.NotAny(x => x))
+            {
+                falseAction.ThrowIfNull(() => trueAction);
+                falseAction(parameter1, parameter2);
+            }
+            else if (trueAction != null)
+                trueAction(parameter1, parameter2);
+        }
+
+        /// <summary>
+        ///     Executes the specified action if the given Boolean values are false,
+        ///     otherwise it executes the specified true action, if one is specified.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">False action can not be null, if all values are false.</exception>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="trueAction">The action to execute if any of the given values is true.</param>
+        /// <param name="falseAction">The action to execute if the given values are false.</param>
+        /// <param name="parameter1">The first parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter2">The second parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter3">The third parameter to pass to the action with gets executed.</param>
+        /// <param name="values">The Boolean values to check.</param>
+        public static void ExecuteIfFalse<T1, T2, T3>(this Action<T1, T2, T3> falseAction, T1 parameter1, T2 parameter2,
+            T3 parameter3, Action<T1, T2, T3> trueAction = null, params Boolean[] values)
+        {
+            if (values.NotAny(x => x))
+            {
+                falseAction.ThrowIfNull(() => trueAction);
+                falseAction(parameter1, parameter2, parameter3);
+            }
+            else if (trueAction != null)
+                trueAction(parameter1, parameter2, parameter3);
+        }
+
+        /// <summary>
+        ///     Executes the specified action if the given Boolean values are false,
+        ///     otherwise it executes the specified true action, if one is specified.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">False action can not be null, if all values are false.</exception>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="trueAction">The action to execute if any of the given values is true.</param>
+        /// <param name="falseAction">The action to execute if the given values are false.</param>
+        /// <param name="parameter1">The first parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter2">The second parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter3">The third parameter to pass to the action with gets executed.</param>
+        /// <param name="parameter4">The fourth parameter to pass to the action with gets executed.</param>
+        /// <param name="values">The Boolean values to check.</param>
+        public static void ExecuteIfFalse<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> falseAction, T1 parameter1,
+            T2 parameter2, T3 parameter3, T4 parameter4, Action<T1, T2, T3, T4> trueAction = null,
+            params Boolean[] values)
+        {
+            if (values.NotAny(x => x))
+            {
+                falseAction.ThrowIfNull(() => trueAction);
+                falseAction(parameter1, parameter2, parameter3, parameter4);
+            }
+            else if (trueAction != null)
+                trueAction(parameter1, parameter2, parameter3, parameter4);
+        }
+    }
+}
--- PortableExtensions/System.Boolean/Boolean.IfTrue.cs
+++ PortableExtensions/System.Boolean/Boolean.IfTrue.cs
@@ -1,6 +1,7 @@
-﻿#region Using
+﻿#region Usings
 
 using System;
+using System.Linq;
 
 #endregion
 
@@ -18,16 +19,16 @@ namespace PortableExtensions
         /// <exception cref="ArgumentNullException">Action can not be null, if the value is true.</exception>
         /// <param name="value">The Boolean to check.</param>
         /// <param name="action">The action to execute if the given value is true.</param>
-        /// <param name="alternativeAction">The action to execute if the given value is false.></param>
+        /// <param name="alternativeAction">The action to execute if the given value is false.</param>
         /// <returns>Returns the given boolean value.</returns>
-        public static Boolean IfTrue( this Boolean value, Action action, Action alternativeAction = null )
+        public static Boolean IfTrue(this Boolean value, Action action, Action alternativeAction = null)
         {
-            if ( value )
+            if (value)
             {
-                action.ThrowIfNull( () => action );
+                action.ThrowIfNull(() => action);
                 action();
             }
-            else if ( alternativeAction != null )
+            else if (alternativeAction != null)
                 alternativeAction();
 
             return value;
@@ -42,18 +43,18 @@ namespace PortableExtensions
         /// <param name="value">The Boolean to check.</param>
         /// <param name="parameter">The parameter to pass to the action with gets executed.</param>
         /// <param name="action">The action to execute if the given value is true.</param>
-        /// <param name="alternativeAction">The action to execute if the given value is false.></param>
+        /// <param name="alternativeAction">The action to execute if the given value is false.</param>
         /// <returns>Returns the given boolean value.</returns>
-        public static Boolean IfTrue<T>( this Boolean value, T parameter, Action<T> action,
-                                         Action<T> alternativeAction = null )
+        public static Boolean IfTrue<T>(this Boolean value, T parameter, Action<T> action,
+            Action<T> alternativeAction = null)
         {
-            if ( value )
+            if (value)
             {
-                action.ThrowIfNull( () => action );
-                action( parameter );
+                action.ThrowIfNull(() => action);
+                action(parameter);
             }
-            else if ( alternativeAction != null )
-                alternativeAction( parameter );
+            else if (alternativeAction != null)
+                alternativeAction(parameter);
 
             return value;
         }
@@ -69,18 +70,18 @@ namespace PortableExtensions
         /// <param name="parameter1">The first parameter to pass to the action with gets executed.</param>
         /// <param name="parameter2">The second parameter to pass to the action with gets executed.</param>
         /// <param name="action">The action to execute if the given value is true.</param>
-        /// <param name="alternativeAction">The action to execute if the given value is false.></param>
+        /// <param name="alternativeAction">The action to execute if the given value is false.</param>
         /// <returns>Returns the given boolean value.</returns>
-        public static Boolean IfTrue<T1, T2>( this Boolean value, T1 parameter1, T2 parameter2, Action<T1, T2> action,
-                                              Action<T1, T2> alternativeAction = null )
+        public static Boolean IfTrue<T1, T2>(this Boolean value, T1 parameter1, T2 parameter2, Action<T1, T2> action,
+            Action<T1, T2> alternativeAction = null)
         {
-            if ( value )
+            if (value)
             {
-                action.ThrowIfNull( () => action );
-                action( parameter1, parameter2 );
+                action.ThrowIfNull(() => action);
+                action(parameter1, parameter2);
             }
-            else if ( alternativeAction != null )
-                alternativeAction( parameter1, parameter2 );
+            else if (alternativeAction != null)
+                alternativeAction(parameter1, parameter2);
 
             return value;
         }
@@ -98,18 +99,18 @@ namespace PortableExtensions
         /// <param name="parameter2">The second parameter to pass to the action with gets executed.</param>
         /// <param name="parameter3">The third parameter to pass to the action with gets executed.</param>
         /// <param name="action">The action to execute if the given value is true.</param>
-        /// <param name="alternativeAction">The action to execute if the given value is false.></param>
+        /// <param name="alternativeAction">The action to execute if the given value is false.</param>
         /// <returns>Returns the given boolean value.</returns>
-        public static Boolean IfTrue<T1, T2, T3>( this Boolean value, T1 parameter1, T2 parameter2, T3 parameter3,
-                                                  Action<T1, T2, T3> action, Action<T1, T2, T3> alternativeAction = null )
+        public static Boolean IfTrue<T1, T2, T3>(this Boolean value, T1 parameter1, T2 parameter2, T3 parameter3,
+            Action<T1, T2, T3> action, Action<T1, T2, T3> alternativeAction = null)
         {
-            if ( value )
+            if (value)
             {
-                action.ThrowIfNull( () => action );
-                action( parameter1, parameter2, parameter3 );
+                action.ThrowIfNull(() => action);
+                action(parameter1, parameter2, parameter3);
             }
-            else if ( alternativeAction != null )
-                alternativeAction( parameter1, parameter2, parameter3 );
+            else if (alternativeAction != null)
+                alternativeAction(parameter1, parameter2, parameter3);
 
             return value;
         }
@@ -129,19 +130,19 @@ namespace PortableExtensions
         /// <param name="parameter3">The third parameter to pass to the action with gets executed.</param>
         /// <param name="parameter4">The fourth parameter to pass to the action with gets executed.</param>
         /// <param name="action">The action to execute if the given value is true.</param>
-        /// <param name="alternativeAction">The action to execute if the given value is false.></param>
+        /// <param name="alternativeAction">The action to execute if the given value is false.</param>
         /// <returns>Returns the given boolean value.</returns>
-        public static Boolean IfTrue<T1, T2, T3, T4>( this Boolean value, T1 parameter1, T2 parameter2, T3 parameter3,
-                                                      T4 parameter4, Action<T1, T2, T3, T4> action,
-                                                      Action<T1, T2, T3, T4> alternativeAction = null )
+        public static Boolean IfTrue<T1, T2, T3, T4>(this Boolean value, T1 parameter1, T2 parameter2, T3 parameter3,
+            T4 parameter4, Action<T1, T2, T3, T4> action,
+            Action<T1, T2, T3, T4> alternativeAction = null)
         {
-            if ( value )
+            if (value)
             {
-                action.ThrowIfNull( () => action );
-                action( parameter1, parameter2, parameter3, parameter4 );
+                action.ThrowIfNull(() => action);
+                action(parameter1, parameter2, parameter3, parameter4);
             }
-            else if ( alternativeAction != null )
-                alternativeAction( parameter1, parameter2, parameter3, parameter4 );
+            else if (alternativeAction != null)
+                alternativeAction(parameter1, parameter2, parameter3, parameter4);
 
             return value;
         }
