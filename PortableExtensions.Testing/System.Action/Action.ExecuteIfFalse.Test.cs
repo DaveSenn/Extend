5961c3f8646d6fb0d4301f6b37fcca6c6e3474a7
Dave Senn <dave.jon.senn@gmail.com>
2014-07-09 22:56:14 +0200

    Add unit tests for Action.ExecuteIfFalse

---
+++ PortableExtensions.Testing/System.Action/Action.ExecuteIfFalse.Test.cs
@@ -0,0 +1,133 @@
+﻿#region Usings
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class ActionExTest
+    {
+        [Test]
+        public void ExecuteIfFalseTestCase()
+        {
+            //Case 1
+            var falseActionExecuted = false;
+            var trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(() => falseActionExecuted = true, null, false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 2
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(() => falseActionExecuted = true, () => trueActionExecuted = true, false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 3
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(() => falseActionExecuted = true, () => trueActionExecuted = true, true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsTrue(trueActionExecuted);
+
+            //Case 4
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(() => falseActionExecuted = true, null, true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 5
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(null, null, true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+        }
+
+        [Test]
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void ExecuteIfFalseTestCaseNullCheck()
+        {
+            ActionEx.ExecuteIfFalse(null, null, false, false);
+        }
+
+        [Test]
+        public void ExecuteIfFalseTestCase1()
+        {
+            var parameter = RandomValueEx.GetRandomString();
+
+            //Case 1
+            var falseActionExecuted = false;
+            var trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                x => falseActionExecuted = x == parameter, 
+                parameter, 
+                null, 
+                false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 2
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                x => falseActionExecuted = x == parameter,
+                parameter,
+                x => trueActionExecuted = x == parameter,
+                false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 3
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                x => falseActionExecuted = x == parameter,
+                parameter,
+                x => trueActionExecuted = x == parameter, 
+                true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsTrue(trueActionExecuted);
+
+            //Case 4
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                x => falseActionExecuted = x == parameter,
+                parameter,
+                null,
+                true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 5
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(null, parameter, null, true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecuteIfFalseTestCase1NullCheck()
+        {
+            ActionEx.ExecuteIfFalse(null, RandomValueEx.GetRandomString(), null, false, false);
+        }
+    }
+}
