73970d3b9da80b0157b2d198ed81e1fdcf77ee15
Dave Senn <dave.jon.senn@gmail.com>
2014-07-09 22:40:27 +0200

    Add unit test for DateTime.ToMonthString

---
+++ PortableExtensions.Testing/System.DateTime/ToDateTimeFormat/DateTime.ToMonthString.Test.cs
@@ -0,0 +1,41 @@
+﻿#region Usings
+
+using System;
+using System.Globalization;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class DateTimeExTest
+    {
+        [TestCase]
+        public void ToMonthStringTestCase()
+        {
+            var dateTime = DateTime.Now;
+            var expected = dateTime.ToString("MMMM");
+            var actual = dateTime.ToMonthString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void ToMonthStringTestCase1()
+        {
+            var dateTime = DateTime.Now;
+            var expected = dateTime.ToString("MMMM", DateTimeFormatInfo.CurrentInfo);
+            var actual = dateTime.ToMonthString(DateTimeFormatInfo.CurrentInfo);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void ToMonthStringTestCase2()
+        {
+            var dateTime = DateTime.Now;
+            var expected = dateTime.ToString("MMMM", new CultureInfo("de-CH"));
+            var actual = dateTime.ToMonthString(new CultureInfo("de-CH"));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
