91c03065ec5de27b655bd1f4f76539aafd40b18b
Dave Senn <dave.jon.senn@gmail.com>
2014-07-09 22:31:20 +0200

    Add method DateTime.ToMonthString

---
+++ PortableExtensions/System.DateTime/ToDateTimeFormat/DateTime.ToMonthString.cs
@@ -0,0 +1,53 @@
+﻿#region Using
+
+using System;
+using System.Globalization;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="DateTime" />.
+    /// </summary>
+    public static partial class DateTimeEx
+    {
+        /// <summary>
+        ///     Converts the DateTime value to a month string.
+        /// </summary>
+        /// <param name="dateTime">The DateTime value to convert.</param>
+        /// <returns>The given value converted to a year month string.</returns>
+        public static String ToMonthString(this DateTime dateTime)
+        {
+            return dateTime.ToString("MMMM", DateTimeFormatInfo.CurrentInfo);
+        }
+
+        /// <summary>
+        ///     Converts the DateTime value to a month string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The format info can not be null.</exception>
+        /// <param name="dateTime">The DateTime value to convert.</param>
+        /// <param name="formatInfo">The date time format info.</param>
+        /// <returns>The given value converted to year month string.</returns>
+        public static String ToMonthString(this DateTime dateTime, DateTimeFormatInfo formatInfo)
+        {
+            formatInfo.ThrowIfNull(() => formatInfo);
+
+            return dateTime.ToString("MMMM", formatInfo);
+        }
+
+        /// <summary>
+        ///     Converts the DateTime value to a month string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The culture can not be null.</exception>
+        /// <param name="dateTime">The DateTime value to convert.</param>
+        /// <param name="culture">The culture.</param>
+        /// <returns>The given value converted to a year month string.</returns>
+        public static String ToMonthString(this DateTime dateTime, CultureInfo culture)
+        {
+            culture.ThrowIfNull(() => culture);
+
+            return dateTime.ToString("MMMM", culture);
+        }
+    }
+}
