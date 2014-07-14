aac2e6a0c9cd8bb6ae4e27cad8af43133a86a4c0
Dave Senn <dave.jon.senn@gmail.com>
2014-07-14 06:55:18 +0200

    Add new unit test¨

--- PortableExtensions.Testing/System.Action/Action.ExecuteIfFalse.Test.cs
+++ PortableExtensions.Testing/System.Action/Action.ExecuteIfFalse.Test.cs
@@ -202,7 +202,6 @@ namespace PortableExtensions.Testing
             ActionEx.ExecuteIfFalse(null, RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), null, false, false);
         }
 
-
         [Test]
         public void ExecuteIfFalseTestCase3()
         {
@@ -274,6 +273,80 @@ namespace PortableExtensions.Testing
         public void ExecuteIfFalseTestCase3NullCheck()
         {
             ActionEx.ExecuteIfFalse(null, RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), null, false, false);
+        }
+
+        [Test]
+        public void ExecuteIfFalseTestCase4()
+        {
+            var parameter1 = RandomValueEx.GetRandomString();
+            var parameter2 = RandomValueEx.GetRandomString();
+            var parameter3 = RandomValueEx.GetRandomString();
+            var parameter4 = RandomValueEx.GetRandomString();
+
+            //Case 1
+            var falseActionExecuted = false;
+            var trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                (p1, p2, p3, p4) => falseActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                parameter1, parameter2, parameter3, parameter4,
+                null,
+                false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 2
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                (p1, p2, p3, p4) => falseActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                parameter1, parameter2, parameter3, parameter4,
+                (p1, p2, p3, p4) => trueActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                false, false);
+
+            Assert.IsTrue(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 3
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                (p1, p2, p3, p4) => falseActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                parameter1, parameter2, parameter3, parameter4,
+                (p1, p2, p3, p4) => trueActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsTrue(trueActionExecuted);
+
+            //Case 4
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(
+                (p1, p2, p3, p4) => falseActionExecuted = p1 == parameter1 && p2 == parameter2 && p3 == parameter3 && p4 == parameter4,
+                parameter1, parameter2, parameter3, parameter4,
+                null,
+                true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+
+            //Case 5
+            falseActionExecuted = false;
+            trueActionExecuted = false;
+            ActionEx.ExecuteIfFalse(null,
+                parameter1, parameter2, parameter3, parameter4,
+                null, true, false);
+
+            Assert.IsFalse(falseActionExecuted);
+            Assert.IsFalse(trueActionExecuted);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecuteIfFalseTestCase4NullCheck()
+        {
+            ActionEx.ExecuteIfFalse(null, RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), RandomValueEx.GetRandomString(), null, false, false);
         }
     }
 }
