86e6c8c3a89fc53a7dfed9884d2ee76af70ffa16
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 20:28:06 +0200

    Add unit test for Array.Populate and Object.ToSingleItemArray + Cleanup

---
+++ PortableExtensions.Testing/System.Object/Generic/Object.Generic.ToSingleItemArray.Test.cs
@@ -0,0 +1,23 @@
+﻿#region Usings
+
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class ObjectExTest
+    {
+        [Test]
+        public void ToSingleItemArrayTestCase()
+        {
+            var item = RandomValueEx.GetRandomString();
+
+            var actual = item.ToSingleItemArray();
+
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(item, actual[0]);
+        }
+    }
+}
