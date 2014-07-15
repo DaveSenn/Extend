93eb4257ddeddea3fef500dd0bd737cc66578cff
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 20:12:45 +0200

    Add Array.Populate

---
+++ PortableExtensions/System.Array/System.Array.Generic/Array.Generic.Populate.cs
@@ -0,0 +1,30 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="Array" />.
+    /// </summary>
+    public static partial class ArrayEx
+    {
+        /// <summary>
+        ///     Populaes the given array with the specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the array.</typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="value">The value.</param>
+        public static T[] Populate<T>(this T[] array, T value)
+        {
+            array.ThrowIfNull(() => array);
+
+            for (var i = 0; i < array.Length; i++)
+                array[i] = value;
+
+            return array;
+        }
+    }
+}
