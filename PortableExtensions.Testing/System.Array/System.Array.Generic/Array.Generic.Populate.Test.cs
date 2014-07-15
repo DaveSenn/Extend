1dfe201171dd3f734fb399789d93e8cc07a46cb9
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 20:13:24 +0200

    Add unit tests for Array.Populate

---
+++ PortableExtensions.Testing/System.Array/System.Array.Generic/Array.Generic.Populate.Test.cs
@@ -0,0 +1,28 @@
+﻿#region Usings
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class ArrayExTest
+    {
+        [Test]
+        public void PopulateTestCase()
+        {
+            var array = new Int32[10];
+            var actual = array.Populate(100);
+
+            Assert.AreSame(array, actual);
+            Assert.AreEqual(10, array.Length);
+            for (var i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(100, array[i]);
+                Assert.AreEqual(100, actual[i]);
+            }
+        }
+    }
+}
