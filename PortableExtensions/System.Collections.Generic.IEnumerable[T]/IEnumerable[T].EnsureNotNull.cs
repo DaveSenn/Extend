0cebf3e45dafa9900895578a8a6d42acc4a1e6fe
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 21:46:09 +0200

    Add method IEnumerable.EnsureNotNull and cleanup

---
+++ PortableExtensions/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].EnsureNotNull.cs
@@ -0,0 +1,28 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="IEnumerable{T}" />.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static partial class IEnumerableTEx
+    {
+        /// <summary>
+        ///     Ensures that the given <see cref="IEnumerable{T}" /> is not null.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the IEnumerable.</typeparam>
+        /// <param name="enumerable">The IEnumerable.</param>
+        /// <returns>Returns the given IEnumerable if it's not null, otherwise an empty array of type T.</returns>
+        public static IEnumerable<T> EnsureNotNull<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable ?? new T[0];
+        }
+    }
+}
