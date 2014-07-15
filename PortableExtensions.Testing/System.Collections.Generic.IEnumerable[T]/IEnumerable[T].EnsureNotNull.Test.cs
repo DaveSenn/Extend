a2f89d9c424279d56535f5bfc1caa4a71f146589
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 21:47:21 +0200

    Add unit test for IEnumerable.EnsureNotNull and IsNullOrEmpty + cleanup

---
+++ PortableExtensions.Testing/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].EnsureNotNull.Test.cs
@@ -0,0 +1,44 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class IEnumerableTExTest
+    {
+        [TestCase]
+        public void EnsureNotNullTestCase()
+        {
+            List<String> list = null;
+            var actual = list.EnsureNotNull();
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(0, actual.Count());
+            Assert.IsNull(list);
+        }
+
+        [TestCase]
+        public void EnsureNotNullTestCase1()
+        {
+            var list = new List<String>
+            {
+                "1",
+                "2",
+                "3"
+            };
+            var actual = list.EnsureNotNull();
+
+            Assert.AreEqual(3, actual.Count());
+            Assert.AreEqual("1", actual.ElementAt(0));
+            Assert.AreEqual("2", actual.ElementAt(1));
+            Assert.AreEqual("3", actual.ElementAt(2));
+            Assert.AreSame(list, actual);
+        }
+    }
+}
