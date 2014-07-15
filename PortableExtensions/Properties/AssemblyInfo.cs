a673e0aa9a8c02e214d6f4382669e0f81f8fe600
Dave Senn <dave.jon.senn@gmail.com>
2014-07-15 22:00:41 +0200

    Increased version number to 1.0.7.0 updated assembly info and nuget package

--- PortableExtensions/Properties/AssemblyInfo.cs
+++ PortableExtensions/Properties/AssemblyInfo.cs
@@ -8,12 +8,16 @@ using System.Resources;
 // General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
 
 [assembly: AssemblyTitle( "PortableExtensions" )]
-[assembly: AssemblyDescription( "Portable class library containing extension methods" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyDescription("A set of .Net extension methods build as portable class library")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany( "Dave Senn" )]
 [assembly: AssemblyProduct( "PortableExtensions" )]
-[assembly: AssemblyCopyright( "Copyright ©  2013" )]
-[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCopyright( "Copyright © Dave Senn 2014" )]
+[assembly: AssemblyTrademark("PortableExtensions")]
 [assembly: AssemblyCulture( "" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
@@ -23,5 +27,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
