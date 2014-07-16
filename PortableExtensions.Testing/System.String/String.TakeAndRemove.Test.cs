76fef09e584eaf5b61c504f462ed7a36d57e5577
unknown <dsenn@Fig.zurich.logisoft.local>
2014-07-16 17:23:25 +0200

    Add unit tests for String.TakeAndRemove

---
+++ PortableExtensions.Testing/System.String/String.TakeAndRemove.Test.cs
@@ -0,0 +1,49 @@
+﻿#region Using
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [TestCase]
+        public void TakeAndRemoveTestCase()
+        {
+            var value = "Test";
+            var actual = 2.TakeAndRemove( ref value );
+
+            Assert.AreEqual( "Te", actual );
+            Assert.AreEqual( "st", value );
+        }
+
+        [TestCase]
+        public void TakeAndRemoveTestCase1()
+        {
+            var value = "Test";
+            var actual = 4.TakeAndRemove(ref value);
+
+            Assert.AreEqual("Test", actual);
+            Assert.AreEqual(String.Empty, value);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TakeAndRemoveTestCaseNullCheck()
+        {
+            String value = null;
+            var actual = 2.TakeAndRemove(ref value);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TakeAndRemoveTestCaseArgumentOutOfRangeException()
+        {
+            var value = "Test";
+            var actual = 5.TakeAndRemove(ref value);
+        }
+    }
+}
