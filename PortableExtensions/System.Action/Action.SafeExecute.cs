3f86759eae0f160a4a71d0f19b5f9fb9a3c287a3
Dave Senn <dave.jon.senn@gmail.com>
2014-07-17 22:32:47 +0200

    Update Action.SaveExecute/Expect

--- PortableExtensions/System.Action/Action.SafeExecute.cs
+++ PortableExtensions/System.Action/Action.SafeExecute.cs
@@ -12,6 +12,74 @@ namespace PortableExtensions
     public static partial class ActionEx
     {
         /// <summary>
+        ///     Executes the given action insode of a try catch block and catches all exceptions.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
+        public static Boolean SafeExecute(this Action action)
+        {
+            return action.SafeExecuteExcept(new Type[0]);
+        }
+
+        /// <summary>
+        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given type.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception.</typeparam>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
+        public static Boolean SafeExecute<TException>(this Action action) where TException : Exception
+        {
+            return action.SafeExecute(new[] { typeof(TException) });
+        }
+
+        /// <summary>
+        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
+        /// </summary>
+        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
+        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
+        public static Boolean SafeExecute<TException1, TException2>(this Action action)
+            where TException1 : Exception
+            where TException2 : Exception
+        {
+            return action.SafeExecute(new[] { typeof(TException1), typeof(TException2) });
+        }
+
+        /// <summary>
+        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
+        /// </summary>
+        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
+        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
+        /// <typeparam name="TException3">The third exception type to catch.</typeparam>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
+        public static Boolean SafeExecute<TException1, TException2, TException3>(this Action action)
+            where TException1 : Exception
+            where TException2 : Exception
+        {
+            return action.SafeExecute(new[] { typeof(TException1), typeof(TException2), typeof(TException3) });
+        }
+
+        /// <summary>
+        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
+        /// </summary>
+        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
+        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
+        /// <typeparam name="TException3">The third exception type to catch.</typeparam>
+        /// <typeparam name="TException4">The fourth exception type to catch.</typeparam>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
+        public static Boolean SafeExecute<TException1, TException2, TException3, TException4>(this Action action)
+            where TException1 : Exception
+            where TException2 : Exception
+        {
+            return
+                action.SafeExecute(new[]
+                {typeof (TException1), typeof (TException2), typeof (TException3), typeof (TException4)});
+        }
+
+        /// <summary>
         ///     Executes the given action insode of a try catch block. Cataches all exception types contained in the given list of
         ///     exception types.
         /// </summary>
@@ -36,74 +104,6 @@ namespace PortableExtensions
                     throw;
                 return false;
             }
-        }
-
-        /// <summary>
-        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given type.
-        /// </summary>
-        /// <typeparam name="TException">The type of the exception.</typeparam>
-        /// <param name="action">The action to execute.</param>
-        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
-        public static Boolean SafeExecute<TException>(this Action action) where TException : Exception
-        {
-            return action.SafeExecuteExcept(new[] {typeof (TException)});
-        }
-
-        /// <summary>
-        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
-        /// </summary>
-        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
-        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
-        /// <param name="action">The action to execute.</param>
-        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
-        public static Boolean SafeExecute<TException1, TException2>(this Action action)
-            where TException1 : Exception
-            where TException2 : Exception
-        {
-            return action.SafeExecuteExcept(new[] {typeof (TException1), typeof (TException2)});
-        }
-
-        /// <summary>
-        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
-        /// </summary>
-        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
-        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
-        /// <typeparam name="TException3">The third exception type to catch.</typeparam>
-        /// <param name="action">The action to execute.</param>
-        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
-        public static Boolean SafeExecute<TException1, TException2, TException3>(this Action action)
-            where TException1 : Exception
-            where TException2 : Exception
-        {
-            return action.SafeExecuteExcept(new[] {typeof (TException1), typeof (TException2), typeof (TException3)});
-        }
-
-        /// <summary>
-        ///     Executes the given action insode of a try catch block. Cataches exceptions of the given types.
-        /// </summary>
-        /// <typeparam name="TException1">The first exception type to catch.</typeparam>
-        /// <typeparam name="TException2">The second exception type to catch.</typeparam>
-        /// <typeparam name="TException3">The third exception type to catch.</typeparam>
-        /// <typeparam name="TException4">The fourth exception type to catch.</typeparam>
-        /// <param name="action">The action to execute.</param>
-        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
-        public static Boolean SafeExecute<TException1, TException2, TException3, TException4>(this Action action)
-            where TException1 : Exception
-            where TException2 : Exception
-        {
-            return
-                action.SafeExecuteExcept(new[]
-                {typeof (TException1), typeof (TException2), typeof (TException3), typeof (TException4)});
-        }
-
-        /// <summary>
-        ///     Executes the given action insode of a try catch block and catches all exceptions.
-        /// </summary>
-        /// <param name="action">The action to execute.</param>
-        /// <returns>Returns true if the action was executed without an exception, otherwise false.</returns>
-        public static Boolean SafeExecute(this Action action)
-        {
-            return action.SafeExecuteExcept(new Type[0]);
         }
     }
 }
