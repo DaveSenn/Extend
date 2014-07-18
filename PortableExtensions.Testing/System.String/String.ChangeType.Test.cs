4cc63d5386afc63ade747bb3136c74931fe18973
Dave Senn <dave.jon.senn@gmail.com>
2014-07-18 16:50:01 +0200

    Add overload for String.ChangeType and tests for it

--- PortableExtensions.Testing/System.String/String.ChangeType.Test.cs
+++ PortableExtensions.Testing/System.String/String.ChangeType.Test.cs
@@ -34,6 +34,34 @@ namespace PortableExtensions.Testing
 
         [Test]
         [ExpectedException( typeof ( ArgumentNullException ) )]
+        public void ChangeTypeTestCase1NullCkeck1()
+        {
+            var actual = "100".ChangeType( typeof ( Int32 ), null );
+        }
+
+        [Test]
+        public void ChangeTypeTestCase2()
+        {
+            var actual = "100".ChangeType<Int32>();
+            Assert.AreEqual( 100, actual );
+        }
+
+        [Test]
+        public void ChangeTypeTestCase3()
+        {
+            var actual = "100".ChangeType<Int32>( CultureInfo.InvariantCulture );
+            Assert.AreEqual( 100, actual );
+        }
+
+        [Test]
+        [ExpectedException( typeof ( ArgumentNullException ) )]
+        public void ChangeTypeTestCase3NullCkeck()
+        {
+            var actual = "100".ChangeType<Int32>( null );
+        }
+
+        [Test]
+        [ExpectedException( typeof ( ArgumentNullException ) )]
         public void ChangeTypeTestCaseNullCkeck()
         {
             var actual = "100".ChangeType( null );
