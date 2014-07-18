eb1862ab3af4e82c0915a51a8f59b4a71ef5694c
Dave Senn <dave.jon.senn@gmail.com>
2014-07-18 16:15:41 +0200

    Add new method String.ChangeType

---
+++ PortableExtensions.Testing/System.String/String.ChangeType.Test.cs
@@ -0,0 +1,42 @@
+﻿#region Using
+
+using System;
+using System.Globalization;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void ChangeTypeTestCase()
+        {
+            var actual = "100".ChangeType( typeof ( Int32 ) );
+            Assert.AreEqual( 100, actual );
+        }
+
+        [Test]
+        public void ChangeTypeTestCase1()
+        {
+            var actual = "100".ChangeType( typeof ( Int32 ), CultureInfo.InvariantCulture );
+            Assert.AreEqual( 100, actual );
+        }
+
+        [Test]
+        [ExpectedException( typeof ( ArgumentNullException ) )]
+        public void ChangeTypeTestCase1NullCkeck()
+        {
+            var actual = "100".ChangeType( null, CultureInfo.InvariantCulture );
+        }
+
+        [Test]
+        [ExpectedException( typeof ( ArgumentNullException ) )]
+        public void ChangeTypeTestCaseNullCkeck()
+        {
+            var actual = "100".ChangeType( null );
+        }
+    }
+}
