ad5f2e91a620957886ba00d89e6765025ce1d769
Dave Senn <dave.jon.senn@gmail.com>
2014-07-18 16:50:01 +0200

    Add overload for String.ChangeType and tests for it

--- PortableExtensions/System.String/String.ChangeType.cs
+++ PortableExtensions/System.String/String.ChangeType.cs
@@ -40,6 +40,7 @@ namespace PortableExtensions
         ///     the specified object.
         /// </summary>
         /// <exception cref="ArgumentNullException">type can not be null.</exception>
+        /// <exception cref="ArgumentNullException">formatProvider can not be null.</exception>
         /// <exception cref="OverflowException">value represents a number that is out of the range of conversionType.</exception>
         /// <exception cref="FormatException">value is not in a format recognized by conversionType.</exception>
         /// <exception cref="InvalidCastException">
@@ -56,9 +57,60 @@ namespace PortableExtensions
         /// </returns>
         public static Object ChangeType( this String value, Type type, IFormatProvider formatProvider )
         {
-            type.ThrowIfNull( () => type );
+            type.ThrowIfNull(() => type);
+            formatProvider.ThrowIfNull(() => formatProvider);
 
             return Convert.ChangeType( value, type, formatProvider );
+        }
+
+        /// <summary>
+        ///     Returns an object of the specified type and whose value is equivalent to
+        ///     the specified object.
+        /// </summary>
+        /// <typeparam name="T">The target type.</typeparam>
+        /// <exception cref="ArgumentNullException">type can not be null.</exception>
+        /// <exception cref="OverflowException">value represents a number that is out of the range of conversionType.</exception>
+        /// <exception cref="FormatException">value is not in a format recognized by conversionType.</exception>
+        /// <exception cref="InvalidCastException">
+        ///     This conversion is not supported. -or-value is null and conversionType is
+        ///     a value type.-or-value does not implement the System.IConvertible interface.
+        /// </exception>
+        /// <param name="value">An object that implements the System.IConvertible interface.</param>
+        /// <returns>
+        ///     An object whose type is conversionType and whose value is equivalent to value.-or-A
+        ///     null reference (Nothing in Visual Basic), if value is null and conversionType
+        ///     is not a value type.
+        /// </returns>
+        public static T ChangeType<T>( this String value )
+        {
+            return (T) Convert.ChangeType( value, typeof ( T ) );
+        }
+
+        /// <summary>
+        ///     Returns an object of the specified type and whose value is equivalent to
+        ///     the specified object.
+        /// </summary>
+        /// <typeparam name="T">The target type.</typeparam>
+        /// <exception cref="ArgumentNullException">type can not be null.</exception>
+        /// <exception cref="ArgumentNullException">formatProvider can not be null.</exception>
+        /// <exception cref="OverflowException">value represents a number that is out of the range of conversionType.</exception>
+        /// <exception cref="FormatException">value is not in a format recognized by conversionType.</exception>
+        /// <exception cref="InvalidCastException">
+        ///     This conversion is not supported. -or-value is null and conversionType is
+        ///     a value type.-or-value does not implement the System.IConvertible interface.
+        /// </exception>
+        /// <param name="value">An object that implements the System.IConvertible interface.</param>
+        /// <param name="formatProvider"> An object that supplies culture-specific formatting information.</param>
+        /// <returns>
+        ///     An object whose type is conversionType and whose value is equivalent to value.-or-A
+        ///     null reference (Nothing in Visual Basic), if value is null and conversionType
+        ///     is not a value type.
+        /// </returns>
+        public static T ChangeType<T>( this String value, IFormatProvider formatProvider )
+        {
+            formatProvider.ThrowIfNull(() => formatProvider);
+
+            return (T) Convert.ChangeType( value, typeof ( T ), formatProvider );
         }
     }
 }
