fefd6a78d8bf6480a4a6a608cc20e3a3d39182bb
Dave Senn <dave.jon.senn@gmail.com>
2014-08-12 10:55:31 +0200

    Add new method DateTime.NextWeekDay

---
+++ PortableExtensions.Testing/System.DateTime/DateTime.NextWeekDay.cs
@@ -0,0 +1,30 @@
+﻿#region Using
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class DateTimeExTest
+    {
+        [TestCase]
+        public void NextWeekDayTestCase()
+        {
+            var friday = new DateTime( 2014, 8, 8 );
+            var saturday = new DateTime(2014, 8, 9);
+            var sunday = new DateTime(2014, 8, 10);
+
+            var actual = friday.NextWeekDay();
+            Assert.AreEqual( friday, actual );
+
+            actual = saturday.NextWeekDay();
+            Assert.AreEqual(new DateTime(2014, 8, 11), actual);
+
+            actual = sunday.NextWeekDay();
+            Assert.AreEqual(new DateTime(2014, 8, 11), actual);
+        }
+    }
+}
---
+++ PortableExtensions/System.DateTime/DateTime.NextWeekDay.cs
@@ -0,0 +1,26 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="DateTime" />.
+    /// </summary>
+    public static partial class DateTimeEx
+    {
+        /// <summary>
+        ///     Gets the next week day, based on the given day.
+        /// </summary>
+        /// <param name="day">The day.</param>
+        /// <returns>Returns the next week day (can be <paramref name="day" /> if the given day is a week day).</returns>
+        public static DateTime NextWeekDay( this DateTime day )
+        {
+            while ( day.IsWeekendDay() )
+                day = day.Add( 1.ToDays() );
+            return day;
+        }
+    }
+}
