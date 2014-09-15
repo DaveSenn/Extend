8b618cd11a4a2b6e1ad8d2c3dab3acff5bd476a5
Dave Senn <dave.jon.senn@gmail.com>
2014-09-15 14:11:02 +0200

    Add new extension method DateTime.NextMonthStart

---
+++ PortableExtensions/System.DateTime/DateTime.NextMonthStart.cs
@@ -0,0 +1,24 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="DateTime" />.
+    /// </summary>
+    public static partial class DateTimeEx
+    {
+        /// <summary>
+        ///     Returns the first day of the next month, based on the given date-time value.
+        /// </summary>
+        /// <param name="dateTime">The date-time value.</param>
+        /// <returns>Returns the first day of the next month, based on the given date-time value.</returns>
+        public static DateTime NextMonthStart( this DateTime dateTime )
+        {
+            return dateTime.AddMonths( 1 ).StartOfMonth();
+        }
+    }
+}
