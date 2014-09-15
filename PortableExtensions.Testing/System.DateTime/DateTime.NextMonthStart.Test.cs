89d5e90df2c94c019e2b58f148cd39309521f2ad
Dave Senn <dave.jon.senn@gmail.com>
2014-09-15 14:11:32 +0200

    Add tests for DateTime.NextMonthStart

---
+++ PortableExtensions.Testing/System.DateTime/DateTime.NextMonthStart.Test.cs
@@ -0,0 +1,45 @@
+﻿#region Using
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class DateTimeExTest
+    {
+        [TestCase]
+        public void NextMonthStartTestCase()
+        {
+            var dateTime = DateTime.Now;
+            var actual = dateTime.NextMonthStart();
+
+            var expected = dateTime.Month == 12
+                               ? new DateTime( dateTime.Year + 1, 1, 1 )
+                               : new DateTime( dateTime.Year, dateTime.Month + 1, 1 );
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestCase]
+        public void NextMonthStartTestCase1()
+        {
+            var dateTime = new DateTime( 2014, 08, 12, 12, 12, 5 );
+            var expected = new DateTime(2014, 09, 01);
+
+            var actual = dateTime.NextMonthStart();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void NextMonthStartTestCase2()
+        {
+            var dateTime = new DateTime(2014, 12, 12, 12, 12, 5);
+            var expected = new DateTime(2015, 01, 01);
+
+            var actual = dateTime.NextMonthStart();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
