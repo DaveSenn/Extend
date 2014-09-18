8041f90aff27a116710a184b84c5bfdb50e29815
Dave Senn <dave.jon.senn@gmail.com>
2014-09-18 21:24:54 +0200

    Improve unit tests

--- PortableExtensions.Testing/System.DateTime/ToDateTimeFormat/DateTime.ToLongDateString.Test.cs
+++ PortableExtensions.Testing/System.DateTime/ToDateTimeFormat/DateTime.ToLongDateString.Test.cs
@@ -15,27 +15,27 @@ namespace PortableExtensions.Testing
         public void ToLongDateStringTestCase()
         {
             var dateTime = DateTime.Now;
-            var expected = dateTime.ToString( "D" );
-            var actual = dateTime.ToLongDateString();
-            Assert.AreEqual( expected, actual );
+            var expected = dateTime.ToString("D");
+            var actual = DateTimeEx.ToLongDateString(dateTime);
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ToLongDateStringTestCase1()
         {
             var dateTime = DateTime.Now;
-            var expected = dateTime.ToString( "D", DateTimeFormatInfo.CurrentInfo );
-            var actual = dateTime.ToLongDateString( DateTimeFormatInfo.CurrentInfo );
-            Assert.AreEqual( expected, actual );
+            var expected = dateTime.ToString("D", DateTimeFormatInfo.CurrentInfo);
+            var actual = dateTime.ToLongDateString(DateTimeFormatInfo.CurrentInfo);
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ToLongDateStringTestCase2()
         {
             var dateTime = DateTime.Now;
-            var expected = dateTime.ToString( "D", CultureInfo.InvariantCulture );
-            var actual = dateTime.ToLongDateString( CultureInfo.InvariantCulture );
-            Assert.AreEqual( expected, actual );
+            var expected = dateTime.ToString("D", CultureInfo.InvariantCulture);
+            var actual = dateTime.ToLongDateString(CultureInfo.InvariantCulture);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
