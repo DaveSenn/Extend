ffe2c6772b82449b3df07e1d94a141b8c63f1fca
Dave Senn <dave.jon.senn@gmail.com>
2014-09-18 21:16:39 +0200

    Improve test for DateTime.StartOfWeek

--- PortableExtensions.Testing/System.DateTime/DateTime.StartOfWeek.Test.cs
+++ PortableExtensions.Testing/System.DateTime/DateTime.StartOfWeek.Test.cs
@@ -13,16 +13,39 @@ namespace PortableExtensions.Testing
         [Test]
         public void StartOfWeekTestCase()
         {
-            var dateTime = new DateTime( 2014, 3, 27 );
-            var expected = new DateTime( 2014, 3, 24 );
+            var dateTime = new DateTime(2014, 3, 27);
+            var expected = new DateTime(2014, 3, 24);
             var actual = dateTime.StartOfWeek();
 
-            Assert.AreEqual( expected, actual );
+            Assert.AreEqual(expected, actual);
 
-            expected = new DateTime( 2014, 3, 26 );
-            actual = dateTime.StartOfWeek( DayOfWeek.Wednesday );
+            expected = new DateTime(2014, 3, 26);
+            actual = dateTime.StartOfWeek(DayOfWeek.Wednesday);
 
-            Assert.AreEqual( expected, actual );
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StartOfWeekTestCase1()
+        {
+            var dateTime = new DateTime(2014, 3, 27);
+
+            var expected = new DateTime(2014, 3, 26);
+            var actual = dateTime.StartOfWeek(DayOfWeek.Wednesday);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StartOfWeekTestCase2()
+        {
+            var day = DayOfWeek.Sunday - DayOfWeek.Saturday;
+
+            var week = new DateTime(2014, 09, 21);
+            var expected = new DateTime(2014, 09, 20);
+            var actual = week.StartOfWeek(DayOfWeek.Saturday);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
