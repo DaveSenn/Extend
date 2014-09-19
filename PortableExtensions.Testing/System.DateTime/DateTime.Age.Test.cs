5546a2fb7d76177f307c5183c0588d25e666c3ae
Dave Senn <dave.jon.senn@gmail.com>
2014-09-19 07:01:30 +0200

    Improve unit tests

--- PortableExtensions.Testing/System.DateTime/DateTime.Age.Test.cs
+++ PortableExtensions.Testing/System.DateTime/DateTime.Age.Test.cs
@@ -13,16 +13,57 @@ namespace PortableExtensions.Testing
         [Test]
         public void AgeTestCase()
         {
-            var dateTime = new DateTime( 1980, 1, 1 );
+            var dateTime = new DateTime(1980, 1, 1);
             var expected = DateTime.Now.Year - 1980;
             var actual = dateTime.Age();
-            Assert.AreEqual( expected, actual );
+            Assert.AreEqual(expected, actual);
 
-            dateTime = DateTime.Now.AddYears( -2 ).Add( 1.ToDays() );
+            dateTime = DateTime.Now.AddYears(-2).Add(1.ToDays());
 
             expected = 1;
             actual = dateTime.Age();
-            Assert.AreEqual( expected, actual );
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AgeTestCase1()
+        {
+            var dateTime = DateTime.Now.AddYears(-2).Add(1.ToDays());
+            const Int32 expected = 1;
+
+            var actual = dateTime.Age();
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void AgeTestCase2()
+        {
+            var dateTime = DateTime.Now.AddYears(-1).AddMonths(-3);
+            const Int32 expected = 1;
+
+            var actual = dateTime.Age();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AgeTestCase3()
+        {
+            var dateTime = DateTime.Now.AddDays(1);
+            const Int32 expected = -1;
+
+            var actual = dateTime.Age();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AgeTestCase4()
+        {
+            var dateTime = DateTime.Now.AddYears(3);
+            const Int32 expected = -3;
+
+            var actual = dateTime.Age();
+            Assert.AreEqual(expected, actual);
         }
     }
 }
