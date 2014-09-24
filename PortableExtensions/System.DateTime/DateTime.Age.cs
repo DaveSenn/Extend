d9a2251be829ec7f21c91f53ff47079312f22854
Dave Senn <dave.jon.senn@gmail.com>
2014-09-24 23:27:01 +0200

    Fix bug causing incorrect results when dateTime was >= dateTime.Now + 363d

--- PortableExtensions/System.DateTime/DateTime.Age.cs
+++ PortableExtensions/System.DateTime/DateTime.Age.cs
@@ -14,15 +14,27 @@ namespace PortableExtensions
         /// <summary>
         ///     Calculates the difference between the year of the current and the given date time.
         /// </summary>
+        /// <remarks>
+        /// <paramref name="now"/> can be samller than <paramref name="dateTime"/>, which results in negative results.
+        /// </remarks>
         /// <param name="dateTime">The date time value.</param>
+        /// <param name="now">The 'current' date used to caluculate the age, or null tu use <see cref="DateTime.Now" />.</param>
         /// <returns>The difference between the year of the current and the given date time.</returns>
-        public static Int32 Age( this DateTime dateTime )
+        public static Int32 Age(this DateTime dateTime, DateTime? now = null)
         {
-            if ( DateTime.Today.Month < dateTime.Month
-                 || DateTime.Today.Month == dateTime.Month && DateTime.Today.Day < dateTime.Day )
-                return DateTime.Today.Year - dateTime.Year - 1;
+            var currentDate = now ?? DateTime.Now;
+            
+            if (dateTime.Year == currentDate.Year)
+                return 0;
+            
+            var age = currentDate.Year - dateTime.Year;
 
-            return DateTime.Today.Year - dateTime.Year;
+            if (dateTime > currentDate && (currentDate.Month > dateTime.Month || currentDate.Day > dateTime.Day))
+                age ++;
+            else if (  (currentDate.Month < dateTime.Month || (currentDate.Month == dateTime.Month && currentDate.Day < dateTime.Day)))
+                age--;
+
+            return age;
         }
     }
 }
