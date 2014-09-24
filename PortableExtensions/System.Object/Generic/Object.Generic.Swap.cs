cb5981a331a496a065e44b23dc73da186b3168a1
Dave Senn <dave.jon.senn@gmail.com>
2014-09-25 00:28:28 +0200

    Add swap method

---
+++ PortableExtensions/System.Object/Generic/Object.Generic.Swap.cs
@@ -0,0 +1,41 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="object" />.
+    /// </summary>
+    public static partial class ObjectEx
+    {
+        /// <summary>
+        ///     Swaps the given values.
+        /// </summary>
+        /// <typeparam name="T">The type of the values.</typeparam>
+        /// <param name="value0">The first value.</param>
+        /// <param name="value1">The second value.</param>
+        public static void Swap<T>(this T value0, T value1) where T : class
+        {
+            T temp = value0;
+            value0 = value1;
+            value1 = temp;
+        }
+
+        /// <summary>
+        ///     Swaps the given values.
+        /// </summary>
+        /// <typeparam name="T">The type of the values.</typeparam>
+        /// <param name="obj">An object to cal the extension method on, can be null.</param>
+        /// <param name="value0">The first value.</param>
+        /// <param name="value1">The second value.</param>
+        public static void SwapValue<T>(this Object obj, ref T value0, ref T value1) where T : struct
+        {
+            T temp = value0;
+            value0 = value1;
+            value1 = temp;
+        }
+    }
+}
