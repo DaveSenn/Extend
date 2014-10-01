27ff09a95d23c2b266eadfeebcae56bfa9455e65
Dave Senn <dave.jon.senn@gmail.com>
2014-10-01 23:38:46 +0200

    Improve unit tests

--- PortableExtensions.Testing/System.String/String.Truncate.Test.cs
+++ PortableExtensions.Testing/System.String/String.Truncate.Test.cs
@@ -1,5 +1,6 @@
 ﻿#region Using
 
+using System;
 using NUnit.Framework;
 
 #endregion
@@ -14,9 +15,35 @@ namespace PortableExtensions.Testing
         {
             var actual = "testtest".Truncate( 7 );
             Assert.AreEqual( "test...", actual );
+        }
 
-            actual = "testtest".Truncate( 7, "_-_" );
-            Assert.AreEqual( "test_-_", actual );
+        [Test]
+        public void TruncateTestCase1()
+        {
+            var actual = "testtest".Truncate(7, "_-_");
+            Assert.AreEqual("test_-_", actual);
+        }
+
+        [Test]
+        public void TruncateTestCase2()
+        {
+            var actual = "abc".Truncate(7, "_-_");
+            Assert.AreEqual("abc", actual);
+        }
+
+        [Test]
+        public void TruncateTestCase3()
+        {
+            var actual = String.Empty.Truncate(7, "_-_");
+            Assert.AreEqual(String.Empty, actual);
+        }
+
+        [Test]
+        public void TruncateTestCase4()
+        {
+            String value = null;
+            var actual = value.Truncate(7, "_-_");
+            Assert.AreEqual(value, actual);
         }
     }
 }
