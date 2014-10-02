20e6eb86fef20cc4edde7ac94aa84610025767b6
Dave Senn <dave.jon.senn@gmail.com>
2014-10-02 21:27:26 +0200

    Improve comment

--- PortableExtensions/System.String/Pars/String.TryParsDateTimeExact.cs
+++ PortableExtensions/System.String/Pars/String.TryParsDateTimeExact.cs
@@ -13,53 +13,94 @@ namespace PortableExtensions
     public static partial class StringEx
     {
         /// <summary>
-        ///     replace
+        ///     Converts the specified string representation of a date and time to its DateTime equivalent using the specified
+        ///     format,
+        ///     culture-specific format information, and style.
+        ///     The format of the string representatiomust match the specified format exactly.
+        ///     The method returns a value that indicates whether the conversion succeeded.
         /// </summary>
-        /// <exception cref="ArgumentNullException">The value can not be null.</exception>
-        /// <exception cref="ArgumentNullException">The format can not be null.</exception>
-        /// <exception cref="ArgumentNullException">The format provider can not be null.</exception>
-        /// <param name="value">The string to pars.</param>
-        /// <param name="outValue">The parsed value.</param>
-        /// <param name="format">The format of the date time value in the string</param>
-        /// <param name="formatProvider">The format provider.</param>
-        /// <param name="dateTimeStyle">The date time style.</param>
+        /// <exception cref="ArgumentNullException">value can not be null.</exception>
+        /// <exception cref="ArgumentNullException">format can not be null.</exception>
+        /// <exception cref="ArgumentNullException">format provider can not be null.</exception>
+        /// <param name="value">A <see cref="String" /> containing a date and time to convert.</param>
+        /// <param name="format">The required format of s. See the Remarks section for more information.</param>
+        /// <param name="formatProvider">
+        ///     An object that supplies culture-specific formatting information about
+        ///     <paramref name="value" />.
+        /// </param>
+        /// <param name="dateTimeStyle">
+        ///     A bitwise combination of one or more enumeration values that indicate the permitted format
+        ///     of <paramref name="value" />.
+        /// </param>
+        /// <param name="outValue">
+        ///     When this method returns, contains the s<see cref="DateTime" /> value equivalent to the date and time contained in
+        ///     <paramref name="value" />,
+        ///     if the conversion succeeded, or <see cref="DateTime.MinValue" /> if the conversion failed.
+        ///     The conversion fails if either the s or format parameter is null, is an empty string, or does not contain a date
+        ///     and time that correspond to the pattern specified in format.
+        ///     This parameter is passed uninitialized.
+        /// </param>
         /// <returns>Returns true if the parsing was successful, otherwise false.</returns>
-        public static Boolean TryParsDateTimeExact( this String value,
-                                                    String format,
-                                                    IFormatProvider formatProvider,
-                                                    DateTimeStyles dateTimeStyle,
-                                                    out DateTime outValue )
+        public static Boolean TryParsDateTimeExact(this String value,
+            String format,
+            IFormatProvider formatProvider,
+            DateTimeStyles dateTimeStyle,
+            out DateTime outValue)
         {
-            value.ThrowIfNull( () => value );
-            format.ThrowIfNull( () => format );
-            formatProvider.ThrowIfNull( () => formatProvider );
+            value.ThrowIfNull(() => value);
+            format.ThrowIfNull(() => format);
+            formatProvider.ThrowIfNull(() => formatProvider);
 
-            return DateTime.TryParseExact( value, format, formatProvider, dateTimeStyle, out outValue );
+            return DateTime.TryParseExact(value, format, formatProvider, dateTimeStyle, out outValue);
         }
 
         /// <summary>
-        ///     replace
+        ///     Converts the specified string representation of a date and time to its <see cref="DateTime" /> equivalent using the
+        ///     specified array of formats,
+        ///     culture-specific format information, and style. The format of the string representation must match at least one of
+        ///     the specified formats exactly.
+        ///     The method returns a value that indicates whether the conversion succeeded.
         /// </summary>
-        /// <exception cref="ArgumentNullException">The value can not be null.</exception>
-        /// <exception cref="ArgumentNullException">The formats can not be null.</exception>
-        /// <exception cref="ArgumentNullException">The format provider can not be null.</exception>
-        /// <param name="value">The string to pars.</param>
-        /// <param name="outValue">The parsed value.</param>
-        /// <param name="formats">The formats of the date time value in the string</param>
-        /// <param name="formatProvider">The format provider.</param>
-        /// <param name="dateTimeStyle">The date time style.</param>
+        /// <remarks>
+        ///     The DateTime.TryParseExact(String, String[], IFormatProvider, DateTimeStyles, DateTime) method parses the string
+        ///     representation
+        ///     of a date that matches any one of the patterns assigned to the formats parameter.
+        ///     It is like the DateTime.ParseExact(String, String[], IFormatProvider, DateTimeStyles) method, except the
+        ///     TryParseExact method does not throw an exception if the conversion fails.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">value can not be null.</exception>
+        /// <exception cref="ArgumentNullException">formats can not be null.</exception>
+        /// <exception cref="ArgumentNullException">format provider can not be null.</exception>
+        /// <param name="value">A <see cref="String" /> containing a date and time to convert.</param>
+        /// <param name="formats">An array of allowable formats of s. See the Remarks section for more information.</param>
+        /// <param name="formatProvider">
+        ///     An object that supplies culture-specific formatting information about
+        ///     <paramref name="value" />.
+        /// </param>
+        /// <param name="dateTimeStyle">
+        ///     A bitwise combination of one or more enumeration values that indicate the permitted format
+        ///     of <paramref name="value" />.
+        /// </param>
+        /// <param name="outValue">
+        ///     When this method returns, contains the s<see cref="DateTime" /> value equivalent to the date and time contained in
+        ///     <paramref name="value" />,
+        ///     if the conversion succeeded, or <see cref="DateTime.MinValue" /> if the conversion failed.
+        ///     The conversion fails if either the s or format parameter is null, is an empty string, or does not contain a date
+        ///     and time that correspond to the pattern specified in format.
+        ///     This parameter is passed uninitialized.
+        /// </param>
         /// <returns>Returns true if the parsing was successful, otherwise false.</returns>
-        public static Boolean TryParsDateTimeExact( this String value,
-                                                    String[] formats,
-                                                    IFormatProvider formatProvider,
-                                                    DateTimeStyles dateTimeStyle,
-                                                    out DateTime outValue )
+        public static Boolean TryParsDateTimeExact(this String value,
+            String[] formats,
+            IFormatProvider formatProvider,
+            DateTimeStyles dateTimeStyle,
+            out DateTime outValue)
         {
-            value.ThrowIfNull( () => value );
-            formats.ThrowIfNull( () => formats );
-            formatProvider.ThrowIfNull( () => formatProvider );
+            value.ThrowIfNull(() => value);
+            formats.ThrowIfNull(() => formats);
+            formatProvider.ThrowIfNull(() => formatProvider);
 
-            return DateTime.TryParseExact( value, formats, formatProvider, dateTimeStyle, out outValue );
+            return DateTime.TryParseExact(value, formats, formatProvider, dateTimeStyle, out outValue);
         }
     }
 }
