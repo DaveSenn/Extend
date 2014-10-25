718d11d69832fea6e96cee327737db2bb378c6a1
Dave Senn <dave.jon.senn@gmail.com>
2014-10-25 11:35:59 +0200

    Move specification extensions

---
+++ PortableExtensions/PortableExtensions.ISpecification[T]/ISpecification.And.cs
@@ -0,0 +1,35 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="ISpecification{T}"/>.
+    /// </summary>
+    public static partial class SpecificationEx
+    {
+        /// <summary>
+        /// Combines the current specification with the given expression using an AND link.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">specification can not be null.</exception>
+        /// <exception cref="ArgumentNullException">expression can not be null.</exception>
+        /// <typeparam name="T">The target type of the specification.</typeparam>
+        /// <param name="specification">The current specification.</param>
+        /// <param name="expression">The expression to add.</param>
+        /// <param name="message">The validation error message.</param>
+        /// <returns>Returns the combined specifications.</returns>
+        public static ISpecification<T> And<T> ( this ISpecification<T> specification, Func<T, Boolean> expression, String message = null )
+        {
+            specification.ThrowIfNull(() => specification);
+            expression.ThrowIfNull(() => expression);
+
+            var newSpecification = new ExpressionSpecification<T>(expression, message);
+            return specification.And( newSpecification );
+        }
+    }
+}
---
+++ PortableExtensions/PortableExtensions.ISpecification[T]/ISpecification.XOr.cs
@@ -0,0 +1,29 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="ISpecification{T}"/>.
+    /// </summary>
+    public static partial class SpecificationEx
+    {
+        /// <summary>
+        /// Combines the current specification with the given expression using a XOr link.
+        /// </summary>
+        /// <typeparam name="T">The target type of the specification.</typeparam>
+        /// <param name="specification">The current specification.</param>
+        /// <param name="expression">The expression to add.</param>
+        /// <returns>Returns the combined specifications.</returns>
+        public static ISpecification<T> XOr<T>(this ISpecification<T> specification, Func<T, Boolean> expression)
+        {
+            var newSpecification = new ExpressionSpecification<T>(expression);
+            return specification.XOr(newSpecification);
+        }
+    }
+}
