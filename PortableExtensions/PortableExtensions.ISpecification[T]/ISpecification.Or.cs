c2bc806180d0e7fc345bc72249592dfa6ef4f74f
Dave Senn <dave.jon.senn@gmail.com>
2014-10-25 11:38:11 +0200

    Add message parameter to Or method

--- PortableExtensions/PortableExtensions.ISpecification[T]/ISpecification.Or.cs
+++ PortableExtensions/PortableExtensions.ISpecification[T]/ISpecification.Or.cs
@@ -16,13 +16,19 @@ namespace PortableExtensions
         /// <summary>
         /// Combines the current specification with the given expression using a OR link.
         /// </summary>
+        /// <exception cref="ArgumentNullException">specification can not be null.</exception>
+        /// <exception cref="ArgumentNullException">expression can not be null.</exception>
         /// <typeparam name="T">The target type of the specification.</typeparam>
         /// <param name="specification">The current specification.</param>
         /// <param name="expression">The expression to add.</param>
+        /// <param name="message">The validation error message.</param>
         /// <returns>Returns the combined specifications.</returns>
-        public static ISpecification<T> Or<T>(this ISpecification<T> specification, Func<T, Boolean> expression)
+        public static ISpecification<T> Or<T>(this ISpecification<T> specification, Func<T, Boolean> expression, String message = null)
         {
-            var newSpecification = new ExpressionSpecification<T>(expression);
+            specification.ThrowIfNull(() => specification);
+            expression.ThrowIfNull(() => expression);
+
+            var newSpecification = new ExpressionSpecification<T>(expression, message);
             return specification.Or(newSpecification);
         }
     }
