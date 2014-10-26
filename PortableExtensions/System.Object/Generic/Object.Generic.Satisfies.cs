edb8a9cafecd2a56ea2c7126a7f0cafd82da003b
Dave Senn <dave.jon.senn@gmail.com>
2014-10-26 10:54:44 +0100

    Improve specification related extensions

---
+++ PortableExtensions/System.Object/Generic/Object.Generic.Satisfies.cs
@@ -0,0 +1,29 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="object" />.
+    /// </summary>
+    public static partial class ObjectEx
+    {
+        /// <summary>
+        ///     Checks if the objects satisfies the given specification.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">specification can not be null.</exception>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <param name="obj">The object to check.</param>
+        /// <param name="specification">The specification to use.</param>
+        /// <returns>Returns true if the object satisfies the specification; otherwise, false.</returns>
+        public static Boolean Satisfies<T>(this T obj, ISpecification<T> specification)
+        {
+            specification.ThrowIfNull(() => specification);
+
+            return specification.IsSatisfiedBy(obj);
+        }
+    }
+}
---
+++ PortableExtensions/System.Object/Generic/Object.Generic.SatisfiesWithMessages.cs
@@ -0,0 +1,30 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="object" />.
+    /// </summary>
+    public static partial class ObjectEx
+    {
+        /// <summary>
+        /// Checks if the objects satisfies the given specification.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">specification can not be null.</exception>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <param name="obj">The object to check.</param>
+        /// <param name="specification">The specification to use.</param>
+        /// <returns></returns>
+        public static IEnumerable<String> SatisfiesWithMessages<T>(this T obj, ISpecification<T> specification)
+        {
+            specification.ThrowIfNull(() => specification);
+
+            return specification.IsSatisfiedByWithMessages(obj);
+        }
+    }
+}
