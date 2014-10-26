f940c2cef004fdc7ada4c29acc2c65183a03bc54
Dave Senn <dave.jon.senn@gmail.com>
2014-10-26 10:55:30 +0100

    Add new unit tests

---
+++ PortableExtensions.Testing/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].SpecificationForItems.Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class IEnumerableTExTest
+    {
+        [Test]
+        public void SpecificationForItemsTestCase()
+        {
+            var targets = new List<String>();
+            var actual = targets.SpecificationForItems(x => x.Length > 3);
+
+            var result = actual.IsSatisfiedBy("1234");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SpecificationForItemsTestCase1()
+        {
+            var targets = new List<String>();
+            var actual = targets.SpecificationForItems(x => x.Length > 3);
+
+            var result = actual.IsSatisfiedBy("123");
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void SpecificationForItemsTestCase2()
+        {
+            var targets = new List<String>();
+            var actual = targets.SpecificationForItems(x => x.Length > 3, "msg");
+
+            var result = actual.IsSatisfiedByWithMessages("1234").ToList();
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [Test]
+        public void SpecificationForItemsTestCase3()
+        {
+            var targets = new List<String>();
+            var actual = targets.SpecificationForItems(x => x.Length > 3, "msg");
+
+            var result = actual.IsSatisfiedByWithMessages("123").ToList();
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result.Count(x => x == "msg"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SpecificationForItemsTestCaseNullCheck()
+        {
+            var targets = new List<String>();
+            Func<String, Boolean> expression = null;
+            var actual = targets.SpecificationForItems(expression);
+        }
+    }
+}
---
+++ PortableExtensions.Testing/System.Object/Generic/Object.Generic.SatisfiesWithMessages.Test.cs
@@ -0,0 +1,58 @@
+﻿#region Usings
+
+using System;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class ObjectExTest
+    {
+        [Test]
+        public void SatisfiesWithMessagesTestCase()
+        {
+            var specification = new ExpressionSpecification<String>(x => x.Length > 3)
+                .And(x => x.StartsWith("1"));
+
+            var actual = "1234".SatisfiesWithMessages(specification).ToList();
+            Assert.AreEqual(0,actual.Count);
+        }
+
+        [Test]
+        public void SatisfiesWithMessagesTestCase1()
+        {
+            var specification = new ExpressionSpecification<String>(x => x.Length > 3)
+                .And(x => x.StartsWith("1"));
+
+            var actual = "234".SatisfiesWithMessages(specification).ToList();
+            Assert.AreEqual(2, actual.Count);
+            Assert.IsNull(actual[0]);
+            Assert.IsNull(actual[1]);
+        }
+
+        [Test]
+        public void SatisfiesWithMessagesTestCase2()
+        {
+            var specification = new ExpressionSpecification<String>(x => x.Length > 3, "msg1")
+                .And(x => x.StartsWith("1"), "msg2");
+
+            var actual = "234".SatisfiesWithMessages(specification).ToList();
+            Assert.AreEqual(2, actual.Count);
+            Assert.AreEqual(1, actual.Count(x => x == "msg1"));
+            Assert.AreEqual(1, actual.Count(x => x == "msg2"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SatisfiesWithMessagesTestCasenullCheck()
+        {
+            ISpecification<String> specification = null;
+
+            "1234".SatisfiesWithMessages(specification).ToList();
+        }
+    }
+}
