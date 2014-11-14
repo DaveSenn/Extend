263daf68fb18a7598f1e29ec9f81b5998197459b
Dave Senn <dave.jon.senn@gmail.com>
2014-11-14 16:36:32 +0100

    Add new tests, improve tree node

--- PortableExtensions.Testing/Utilities/Tree/TreeNodeTest.cs
+++ PortableExtensions.Testing/Utilities/Tree/TreeNodeTest.cs
@@ -11,7 +11,7 @@ namespace PortableExtensions.Testing
     public class TreeNodeTest
     {
         /// <summary>
-        /// Constructor with no parameters
+        ///     Constructor with no parameters
         /// </summary>
         [Test]
         public void CtorTestCase()
@@ -27,30 +27,30 @@ namespace PortableExtensions.Testing
         }
 
         /// <summary>
-        /// Constructor with the following parameters: value
+        ///     Constructor with the following parameters: value
         /// </summary>
         [Test]
         public void CtorTestCase1()
         {
             var value = RandomValueEx.GetRandomString();
 
-            var target = new TreeNode<String>(value);
-            Assert.AreEqual(value, target.Value);
-            Assert.IsNull(target.Parent);
-            Assert.AreEqual(0, target.Children.Count);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.SearchTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.AncestorsTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.DescendantsTraversalDirection);
+            var target = new TreeNode<String>( value );
+            Assert.AreEqual( value, target.Value );
+            Assert.IsNull( target.Parent );
+            Assert.AreEqual( 0, target.Children.Count );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DescendantsTraversalDirection );
         }
 
         /// <summary>
-        /// Constructor with the following parameters: parent
+        ///     Constructor with the following parameters: parent
         /// </summary>
         [Test]
         public void CtorTestCase2()
         {
-            var parent = new TreeNode<String>()
+            var parent = new TreeNode<String>
             {
                 AncestorsTraversalDirection = TreeTraversalDirection.TopDown,
                 DisposeTraversalDirection = TreeTraversalDirection.BottomUp,
@@ -58,48 +58,144 @@ namespace PortableExtensions.Testing
                 SearchTraversalDirection = TreeTraversalDirection.BottomUp
             };
 
-            var target = new TreeNode<String>(parent);
-            Assert.IsNull(target.Value);
-            Assert.AreSame(parent, target.Parent);
-            Assert.AreEqual(0, target.Children.Count);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.SearchTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.TopDown, target.AncestorsTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.TopDown, target.DescendantsTraversalDirection);
+            var target = new TreeNode<String>( parent );
+            Assert.IsNull( target.Value );
+            Assert.AreSame( parent, target.Parent );
+            Assert.AreEqual( 0, target.Children.Count );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.DescendantsTraversalDirection );
         }
 
         /// <summary>
-        /// Constructor with the following parameters: children
+        ///     Constructor with the following parameters: children
         /// </summary>
         [Test]
         public void CtorTestCase3()
         {
             var exParent = new TreeNode<String>();
-            var children = new TreeNodeCollection<String>(exParent)
+            var children = new TreeNodeCollection<String>( exParent )
             {
                 "Item0",
                 "Item1",
                 "Item2"
             };
+            children.ForEach( x => x.DisposeTraversalDirection = TreeTraversalDirection.TopDown );
 
-            var target = new TreeNode<String>(children);
-            Assert.IsNull(target.Value);
-            Assert.IsNull(target.Parent);
-            Assert.AreEqual(3, target.Children.Count);
+            var target = new TreeNode<String>( children );
+            Assert.IsNull( target.Value );
+            Assert.IsNull( target.Parent );
+
+            Assert.AreEqual( 3, target.Children.Count );
             target.Children.ForEach( x => Assert.AreSame( target, x.Parent ) );
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.SearchTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.AncestorsTraversalDirection);
-            Assert.AreEqual(TreeTraversalDirection.BottomUp, target.DescendantsTraversalDirection);
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.DisposeTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.SearchTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.AncestorsTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.DescendantsTraversalDirection ) );
+
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DescendantsTraversalDirection );
+        }
+
+        /// <summary>
+        ///     Constructor with the following parameters: value, children
+        /// </summary>
+        [Test]
+        public void CtorTestCase4()
+        {
+            var exParent = new TreeNode<String>();
+            var value = RandomValueEx.GetRandomString();
+            var children = new TreeNodeCollection<String>( exParent )
+            {
+                "Item0",
+                "Item1",
+                "Item2"
+            };
+            children.ForEach( x => x.DisposeTraversalDirection = TreeTraversalDirection.TopDown );
+
+            var target = new TreeNode<String>( value, children );
+            Assert.AreEqual( value, target.Value );
+            Assert.IsNull( target.Parent );
+
+            Assert.AreEqual( 3, target.Children.Count );
+            target.Children.ForEach( x => Assert.AreSame( target, x.Parent ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.DisposeTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.SearchTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.AncestorsTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.DescendantsTraversalDirection ) );
+
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DescendantsTraversalDirection );
+        }
+
+        /// <summary>
+        ///     Constructor with the following parameters: value, parent
+        /// </summary>
+        [Test]
+        public void CtorTestCase5()
+        {
+            var value = RandomValueEx.GetRandomString();
+            var parent = new TreeNode<String>
+            {
+                AncestorsTraversalDirection = TreeTraversalDirection.TopDown,
+                DisposeTraversalDirection = TreeTraversalDirection.BottomUp,
+                DescendantsTraversalDirection = TreeTraversalDirection.TopDown,
+                SearchTraversalDirection = TreeTraversalDirection.BottomUp
+            };
+
+            var target = new TreeNode<String>( value, parent );
+            Assert.AreEqual( value, target.Value );
+            Assert.AreSame( parent, target.Parent );
+            Assert.AreEqual( 0, target.Children.Count );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.DescendantsTraversalDirection );
+        }
+
+        /// <summary>
+        ///     Constructor with the following parameters: children
+        /// </summary>
+        [Test]
+        public void CtorTestCase6()
+        {
+            var value = RandomValueEx.GetRandomString();
+            var parent = new TreeNode<String>
+            {
+                AncestorsTraversalDirection = TreeTraversalDirection.TopDown,
+                DisposeTraversalDirection = TreeTraversalDirection.BottomUp,
+                DescendantsTraversalDirection = TreeTraversalDirection.TopDown,
+                SearchTraversalDirection = TreeTraversalDirection.BottomUp
+            };
+            var exParent = new TreeNode<String>();
+            var children = new TreeNodeCollection<String>( exParent )
+            {
+                "Item0",
+                "Item1",
+                "Item2"
+            };
+            children.ForEach( x => x.DisposeTraversalDirection = TreeTraversalDirection.TopDown );
+
+            var target = new TreeNode<String>( value, parent, children );
+            Assert.AreEqual( value, target.Value );
+            Assert.AreSame( parent, target.Parent );
+
+            Assert.AreEqual( 3, target.Children.Count );
+            target.Children.ForEach( x => Assert.AreSame( target, x.Parent ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.DisposeTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.BottomUp, x.SearchTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.TopDown, x.AncestorsTraversalDirection ) );
+            target.Children.ForEach( x => Assert.AreEqual( TreeTraversalDirection.TopDown, x.DescendantsTraversalDirection ) );
+
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.DisposeTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.BottomUp, target.SearchTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.AncestorsTraversalDirection );
+            Assert.AreEqual( TreeTraversalDirection.TopDown, target.DescendantsTraversalDirection );
         }
     }
 }
-
-/*
-Value = value;
-Parent = parent;
-if ( Parent != null && !Parent.Children.Contains( this ) )
-    Parent.Children.Add( this );
-Children = children ?? new TreeNodeCollection<T>( this );
-DisposeTraversalDirection = TreeTraversalDirection.BottomUp;
- */
