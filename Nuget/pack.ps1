047063033fafd18a3df2731e368283447b9d5d72
Dave Senn <dave.jon.senn@gmail.com>
2015-05-13 22:21:50 +0200

    Improve NuGet pack

--- Nuget/pack.ps1
+++ Nuget/pack.ps1
@@ -1,35 +1,31 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-Write-Host "APPVEYOR_BUILD_VERSION: $env:APPVEYOR_BUILD_VERSION"
-
-#$version = [System.Reflection.Assembly]::LoadFile("$root\PortableExtensions\bin\Release\PortableExtensions.dll").GetName().Version
-#$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-#Write-Host "Setting .nuspec version tag to $versionStr"
-
-#$content = (Get-Content $root\Nuget\PortableExtensions.nuspec) 
-#$content = $content -replace '\$version\$',$versionStr
-#$content | Out-File $root\Nuget\PortableExtensions.compiled.nuspec
-
-#& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.compiled.nuspec
-& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.nuspec -Properties version=$env:APPVEYOR_BUILD_VERSION
-
-
-
+# Load needed assemblies
 Add-Type -AssemblyName System
 Add-Type -AssemblyName System.Reflection
 
+# Get path to repository root
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+Write-Host "Root is set to: $root"
+
+# Load the built assembly
 $assembly = [System.Reflection.Assembly]::LoadFile("$root\PortableExtensions\bin\Release\PortableExtensions.dll")
+# Get assembly name and version
 $name = $assembly.GetName().Name;
 $version = $assembly.GetName().Version;
 
+# Get assembly description
 $type = [System.Reflection.AssemblyDescriptionAttribute]
 $attribute = [System.Reflection.AssemblyDescriptionAttribute]::GetCustomAttribute($assembly, $type);
 $description = $attribute.Description
 
+# Get assembly copyright
 $type = [System.Reflection.AssemblyCopyrightAttribute]
 $attribute = [System.Reflection.AssemblyCopyrightAttribute]::GetCustomAttribute($assembly, $type);
 $copyright = $attribute.Copyright
 
-Write-Host $name
-Write-Host $version
-Write-Host $description
-Write-Host $copyright
+Write-Host "Name: $name"
+Write-Host "Version: $version"
+Write-Host "Description: $description"
+Write-Host "Copyright: $copyright"
+
+# Build the package
+& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.nuspec -Properties "id=$name;version=$version;description=$description;copyright=$copyright;"
