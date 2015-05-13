6785f9f448b0024b0c66f9363c012c4b87f8145f
Dave Senn <dave.jon.senn@gmail.com>
2015-05-13 22:31:46 +0200

    Update version number

--- PortableExtensions/Properties/AssemblyInfo.cs
+++ PortableExtensions/Properties/AssemblyInfo.cs
@@ -5,8 +5,6 @@ using System.Resources;
 
 #endregion
 
-// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
-
 [assembly: AssemblyTitle( "PortableExtensions" )]
 [assembly:
     AssemblyDescription(
@@ -17,7 +15,8 @@ using System.Resources;
 
 [assembly: AssemblyConfiguration( "Debug" )]
 #else
-[assembly: AssemblyConfiguration ( "Release" )]
+
+[assembly: AssemblyConfiguration( "Release" )]
 #endif
 
 [assembly: AssemblyCompany( "Dave Senn" )]
@@ -26,12 +25,5 @@ using System.Resources;
 [assembly: AssemblyTrademark( "PortableExtensions" )]
 [assembly: AssemblyCulture( "" )]
 [assembly: NeutralResourcesLanguage( "en" )]
-
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion( "1.1.1.0" )]
-[assembly: AssemblyFileVersion( "1.1.1.0" )]
+[assembly: AssemblyVersion( "1.1.2.0" )]
+[assembly: AssemblyFileVersion( "1.1.2.0" )]
