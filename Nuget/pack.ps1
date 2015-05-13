2a5afa9af8915eb400c83bc128e8d4d3209247e5
Dave Senn <dave.jon.senn@gmail.com>
2015-05-13 19:22:20 +0200

    experimenting with build

--- Nuget/pack.ps1
+++ Nuget/pack.ps1
@@ -1,11 +1,13 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\PortableExtensions\bin\Release\PortableExtensions.dll").GetName().Version
-$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+Write-Host "Root: $root"
 
-Write-Host "Setting .nuspec version tag to $versionStr"
+#$version = [System.Reflection.Assembly]::LoadFile("$root\PortableExtensions\bin\Release\PortableExtensions.dll").GetName().Version
+#$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+#Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\Nuget\PortableExtensions.nuspec) 
-$content = $content -replace '\$version\$',$versionStr
-$content | Out-File $root\Nuget\PortableExtensions.compiled.nuspec
+#$content = (Get-Content $root\Nuget\PortableExtensions.nuspec) 
+#$content = $content -replace '\$version\$',$versionStr
+#$content | Out-File $root\Nuget\PortableExtensions.compiled.nuspec
 
-& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.compiled.nuspec
+#& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.compiled.nuspec
+& $root\Nuget\Nuget.exe pack $root\Nuget\PortableExtensions.nuspec
