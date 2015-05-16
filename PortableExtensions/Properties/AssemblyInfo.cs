ea46ca84f22498b54fd8486f957cf8a1a1b9f0e0
Dave Senn <dave.jon.senn@gmail.com>
2015-05-16 13:58:55 +0200

    Remove spaces

--- PortableExtensions/Properties/AssemblyInfo.cs
+++ PortableExtensions/Properties/AssemblyInfo.cs
@@ -5,25 +5,25 @@ using System.Resources;
 
 #endregion
 
-[assembly: AssemblyTitle( "PortableExtensions" )]
+[assembly: AssemblyTitle("PortableExtensions")]
 [assembly:
     AssemblyDescription(
         "PortableExtensions is a set of .Net extension methods build as portable class library. " +
-        "PortableExtensions enhance the .Net framework by adding a bunch of methods to increase developer’s productivity." )]
+        "PortableExtensions enhance the .Net framework by adding a bunch of methods to increase developer’s productivity.")]
 
 #if DEBUG
 
-[assembly: AssemblyConfiguration( "Debug" )]
+[assembly: AssemblyConfiguration("Debug")]
 #else
 
-[assembly: AssemblyConfiguration( "Release" )]
+[assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyCompany( "Dave Senn" )]
-[assembly: AssemblyProduct( "PortableExtensions" )]
-[assembly: AssemblyCopyright( "Copyright © Dave Senn 2015" )]
-[assembly: AssemblyTrademark( "PortableExtensions" )]
-[assembly: AssemblyCulture( "" )]
-[assembly: NeutralResourcesLanguage( "en" )]
-[assembly: AssemblyVersion( "1.1.4.0" )]
-[assembly: AssemblyFileVersion( "1.1.4.0" )]
+[assembly: AssemblyCompany("Dave Senn")]
+[assembly: AssemblyProduct("PortableExtensions")]
+[assembly: AssemblyCopyright("Copyright © Dave Senn 2015")]
+[assembly: AssemblyTrademark("PortableExtensions")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
