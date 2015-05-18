af00076bbca1455b776068f68505cba387f25a9f
Dave Senn <dave.jon.senn@gmail.com>
2015-05-18 10:40:54 +0200

    Add new methods

---
+++ PortableExtensions.Testing/System.String/String.ToCultureInfo.Test.cs
@@ -0,0 +1,61 @@
+﻿#region Usings
+
+using System;
+using System.Globalization;
+using NUnit.Framework;
+
+#endregion
+
+namespace PortableExtensions.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void ToCultureInfoTestCase()
+        {
+            const String culture = "en";
+            var actual = culture.ToCultureInfo();
+
+            Assert.AreEqual( culture, actual.Name );
+        }
+
+        [Test]
+        public void ToCultureInfoTestCase1()
+        {
+            const String culture = "de-CH";
+            var actual = culture.ToCultureInfo();
+
+            Assert.AreEqual( culture, actual.Name );
+        }
+
+        [Test]
+        [ExpectedException( typeof (ArgumentNullException) )]
+        public void ToCultureInfoTestCase1NullCheck()
+        {
+            const String culture = null;
+            var actual = culture.ToCultureInfo();
+
+            Assert.AreEqual( culture, actual.Name );
+        }
+
+        [Test]
+        public void ToCultureInfoTestCase2()
+        {
+            var culture = String.Empty;
+            var actual = culture.ToCultureInfo();
+
+            Assert.AreEqual( culture, actual.Name );
+        }
+
+        [Test]
+        [ExpectedException( typeof (CultureNotFoundException) )]
+        public void ToCultureInfoTestCase3()
+        {
+            const String culture = "invalidCultureName";
+            var actual = culture.ToCultureInfo();
+
+            Assert.AreEqual( culture, actual.Name );
+        }
+    }
+}
---
+++ PortableExtensions/System.String/String.SafeToCultureInfo.cs
@@ -0,0 +1,47 @@
+﻿#region Usings
+
+using System;
+using System.Globalization;
+
+#endregion
+
+namespace PortableExtensions
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        ///     Tries to create a new <see cref="CultureInfo" /> with the given name.
+        /// </summary>
+        /// <param name="name">The name of the culture.</param>
+        /// <returns>Returns the <see cref="CultureInfo" /> with the given name, or null if the culture is not supported.</returns>
+        public static CultureInfo SafeToCultureInfo( this String name )
+        {
+            try
+            {
+                return new CultureInfo( name );
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///     Tries to create a new <see cref="CultureInfo" /> with the given name.
+        /// </summary>
+        /// <param name="name">The name of the culture.</param>
+        /// <param name="fallbackCulture">The culture returned if the culture with the given name is not supported.</param>
+        /// <returns>Returns the <see cref="CultureInfo" /> with the given name, or <paramref name="fallbackCulture" />.</returns>
+        public static CultureInfo SafeToCultureInfo( this String name, CultureInfo fallbackCulture )
+        {
+            try
+            {
+                return new CultureInfo( name );
+            }
+            catch
+            {
+                return fallbackCulture;
+            }
+        }
+    }
+}
