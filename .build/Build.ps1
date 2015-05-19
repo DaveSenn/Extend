d6077195a045718f245d143eb88b81b387a1a6bc
Dave Senn <dave.jon.senn@gmail.com>
2015-05-19 23:20:27 +0200

    Experimenting with invoke-build

--- .build/Build.ps1
+++ .build/Build.ps1
@@ -3,51 +3,15 @@ $root = [System.IO.Path]::Combine($currentDir, "..\")
 $packages = [System.IO.Path]::Combine($root, "packages")
 $nuget = [System.IO.Path]::Combine($root, ".tools\NuGet\nuget.exe")
 $solution = [System.IO.Path]::Combine($root, "PortableExtensions.sln")
-<#
-properties {
-    # build variables
-    $framework = "4.5.1"		# .net framework version
-    $configuration = "Release"	# build configuration
-    $script:version = "1.0.0"
-    $script:nugetVersion = "1.0.0"
-    $script:runCoverity = $false
 
-    # directories
-    $base_dir = . resolve-path .\
-    $build_output_dir = "$base_dir\src\csmacnz.CoverityPublisher\bin\$configuration\"
-    $test_results_dir = "$base_dir\TestResults\"
-    $package_dir = "$base_dir\Package\"
-    $archive_dir = "$package_dir" + "Archive"
-    $nuget_pack_dir = "$package_dir" + "Pack"
-
-    # files
-    $sln_file = "$base_dir\src\csmacnz.CoverityPublisher.sln"
-    $nuspec_filename = "PublishCoverity.nuspec"
-    $applicationName = "PublishCoverity"
-}
-#>
-
-Task default -Depends RestorePackages
-
-# Cleans the repository
-Task Clean {
-	Write-Host "Restoring NuGet packages"
-	Write-Host (Get-Location)
-    
-	exec { 
-		Set-Location $root
-		git clean -xdf }
-	Start-Process git -Argumentlist "clean -xdf" -WorkingDirectory $root
-	
-	Write-Host (Get-Location)
+task Build {
+	Write-Host "Build..."
 }
 
-# Task restoring the NuGet packages of the solution
-Task RestorePackages {
-	Write-Host "Restoring NuGet packages"
-    exec { &$nuget restore $solution }
+task Clean {
+	Write-Host "Clean..."
 }
 
-Task Test {
-    Write-Host "Hi"
-}
+task . Build, Clean
+
+#.\packages\Invoke-Build\tools\Invoke-Build .\.build\Build.ps1
