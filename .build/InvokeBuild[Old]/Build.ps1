b3b7dd81ed624e5ab06c0939218b48b3decfdb0d
Dave Senn <dave.jon.senn@gmail.com>
2015-05-22 14:52:33 +0200

    Upadte build

---
+++ .build/InvokeBuild[Old]/Build.ps1
@@ -0,0 +1,72 @@
+$currentDir = split-path -parent $MyInvocation.MyCommand.Definition
+$root = [System.IO.Path]::Combine($currentDir, "..\")
+$outputDirectory = [System.IO.Path]::Combine($root, "Output")
+$packages = [System.IO.Path]::Combine($env:Temp, "packages")
+$nuget = [System.IO.Path]::Combine($root, ".tools\NuGet\nuget.exe")
+$nunit = [System.IO.Path]::Combine($root, ".tools\NUnit-2.6.4\bin\nunit-console.exe")
+$solution = [System.IO.Path]::Combine($root, "PortableExtensions.sln")
+
+task Build {
+	Write-Host "Build..."
+}
+
+# Removes all not tracked files
+task Clean {
+	ExecInDir {
+		Write-Host "Start git clean"
+		git clean -xdf 
+	} $root 0
+}
+
+# Restores all NuGet packages
+task RestorePackages {
+	exec {
+		&$nuget restore $solution
+	}
+}
+
+# Build the project.
+task Build {
+	exec { 
+		MSBuild $solution /t:Build /p:Configuration=Release /verbosity:minimal
+	}
+}
+
+# Run unit tests.
+task Tests {
+	exec { 
+		&$nunit "C:\_git\PortableExtensions\PortableExtensions.Testing\bin\Release\PortableExtensions.Testing.dll"
+	}
+}
+
+# Create NuGet packages
+task CreatePackage {
+	exec { 
+		$nuspecFile = [System.IO.Path]::Combine($currentDir, "Nuget\PortableExtensions.nuspec")
+		$assemblyPath = [System.IO.Path]::Combine($root, "PortableExtensions\bin\Release\PortableExtensions.dll" )
+		$nugetOutput = [System.IO.Path]::Combine($outputDirectory, "Nuget\")
+		
+		$nugetPack = [System.IO.Path]::Combine($currentDir, "Nuget\pack.ps1")	
+		&$nugetPack $assemblyPath $nuspecFile $nuget $nugetOutput
+	}
+}
+
+function ExecInDir([Parameter(Mandatory=$true)][scriptblock]$Command, [Parameter(Mandatory=$true)][string]$Path,  [int[]]$ExitCode=0) {
+	$private:location = Get-Location
+	Set-Location $Path
+	
+	Write-Host "(Run in $private:location)"
+	exec $Command $ExitCode
+	
+	Set-Location $private:location
+}
+
+task . Clean, RestorePackages, Build, Tests, CreatePackage
+
+<#
+
+&([System.IO.Path]::Combine($env:Temp, "packages\Invoke-Build\tools\Invoke-Build.ps1")) -File .\.build\Build.ps1
+
+&"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+
+#>
