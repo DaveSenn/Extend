d0d54354b6a5ac17a7f2422372136cc9d334039e
Dave Senn <dave.jon.senn@gmail.com>
2015-05-22 12:42:20 +0200

    Add support for .Net 4.0

---
+++ .Src/PortableExtensions/System.Enum/Enum.GetValues.cs
@@ -0,0 +1,68 @@
+﻿#region Usings
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+#if PORTABLE45
+using System.Reflection;
+#endif
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="Enum" />.
+    /// </summary>
+    public static partial class EnumEx
+    {
+        /// <summary>
+        ///     Gets the values of the specified enumeration.
+        /// </summary>
+        /// <exception cref="ArgumentException">T must be an enumerated type.</exception>
+        /// <typeparam name="T">The type of the enumeration.</typeparam>
+        /// <returns>All values of the specified enumeration.</returns>
+        public static IEnumerable<T> GetValues<T>() where T : struct
+        {
+            var type = typeof (T);
+#if PORTABLE45
+            if ( !type.GetTypeInfo()
+                      .IsEnum )
+                throw new ArgumentException( "T must be an enumerated type." );
+#elif NET40
+            if ( !type.IsEnum )
+                throw new ArgumentException( "T must be an enumerated type." );
+#endif
+
+            return Enum.GetValues( type )
+                       .OfType<T>();
+        }
+
+        /// <summary>
+        ///     Gets the values of the specified enumeration.
+        /// </summary>
+        /// <remarks>
+        ///     How to cast returned values:
+        ///     values.Cast{Object}();
+        ///     values.Select( x => Convert.ChangeType( x, type ) );
+        /// </remarks>
+        /// <exception cref="ArgumentException">T must be an enumerated type.</exception>
+        /// <param name="type">The type of the enumeration.</param>
+        /// <returns>All values of the specified enumeration.</returns>
+        public static IEnumerable GetValues( Type type )
+        {
+#if PORTABLE45
+            if ( !type.GetTypeInfo()
+                      .IsEnum )
+                throw new ArgumentException( "T must be an enumerated type." );
+#elif NET40
+            if ( !type.IsEnum )
+                throw new ArgumentException( "T must be an enumerated type." );
+#endif
+
+            return Enum.GetValues( type );
+        }
+    }
+}
---
+++ .Src/PortableExtensions/System.Type/Type.GetAttributeDefinitions.cs
@@ -0,0 +1,51 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#if PORTABLE45
+using System.Reflection;
+#endif
+
+#endregion
+
+namespace PortableExtensions
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="Type" />.
+    /// </summary>
+    public static class TypeEx
+    {
+        /// <summary>
+        ///     Gets the attributes of the proprieties of the given type.
+        /// </summary>
+        /// <typeparam name="TAttribute">The type of attributes to return.</typeparam>
+        /// <param name="t">The type to get the attribute definitions from.</param>
+        /// <returns>Returns the attribute definitions of the given type.</returns>
+        public static IEnumerable<AttributeDefinition<TAttribute>> GetAttributeDefinitions<TAttribute>( this Type t )
+            where TAttribute : Attribute
+        {
+#if PORTABLE45
+            return t.GetRuntimeProperties()
+                    .Select( x => new AttributeDefinition<TAttribute>
+                    {
+                        Property = x,
+                        Attributes = x.GetCustomAttributes( typeof (TAttribute), true )
+                                      .Cast<TAttribute>()
+                    } )
+                    .Where( x => x.Attributes.Any() );
+
+#elif NET40
+            return t.GetProperties()
+                    .Select( x => new AttributeDefinition<TAttribute>
+                    {
+                        Property = x,
+                        Attributes = x.GetCustomAttributes( typeof (TAttribute), true )
+                                      .Cast<TAttribute>()
+                    } )
+                    .Where( x => x.Attributes.Any() );
+#endif
+        }
+    }
+}
