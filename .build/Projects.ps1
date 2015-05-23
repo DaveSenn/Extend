9a4b70cd8487f40cc24d78faa78a2960f62f83fc
Dave Senn <dave.jon.senn@gmail.com>
2015-05-23 17:11:23 +0200

    Improve copy

--- .build/Projects.ps1
+++ .build/Projects.ps1
@@ -2,8 +2,8 @@
 Script containing all project-build configuration.
 #>
 $projects = @(
-	@{ Name = "Extend.Net40.sln";    ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Portable"; TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Testing" },
-	@{ Name = "Extend.Portable.sln"; ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Net40";    TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Testing" }
+	@{ Name = "Extend.Net40.sln";    ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Portable"; TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Testing"; NuGetDir = "portable-net45+wp8+win8"; },
+	@{ Name = "Extend.Portable.sln"; ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Net40";    TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Testing"; NuGetDir = "net40"; }
 )
 
 # Get all projects were ShouldBuild is true
