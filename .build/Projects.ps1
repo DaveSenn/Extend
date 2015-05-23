d2322f1b99d6542fc96a016539b521c9d0f31fd4
Dave Senn <dave.jon.senn@gmail.com>
2015-05-23 15:36:07 +0200

    Change project names

--- .build/Projects.ps1
+++ .build/Projects.ps1
@@ -2,9 +2,8 @@
 Script containing all project-build configuration.
 #>
 $projects = @(
-	#@{  Name = "Newtonsoft.Json"; TestsName = "Newtonsoft.Json.Tests"; 	TestsFunction = "NUnitTests"; Constants=""; FinalDir="Net45"; NuGetDir = "net45"; Framework="net-4.0"; Sign=$true }
-	@{ Name = "PortableExtensions.Net40.sln";    ProjectDirectory = "PortableExtensions"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Portable"; TestRunner = "NUnit"; TestDirectory = "PortableExtensions.Testing"; TestProjectName = "PortableExtensions.Net40.Testing" },
-	@{ Name = "PortableExtensions.Portable.sln"; ProjectDirectory = "PortableExtensions"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Net40";    TestRunner = "NUnit"; TestDirectory = "PortableExtensions.Testing"; TestProjectName = "PortableExtensions.Portable.Testing" }
+	@{ Name = "Extend.Net40.sln";    ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Portable"; TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Net40.Testing" },
+	@{ Name = "Extend.Portable.sln"; ProjectDirectory = "Extend"; ShouldBuild = $true; RestoreNuGetPackages = $true; OutputDirectory="Net40";    TestRunner = "NUnit"; TestDirectory = "Extend.Testing"; TestProjectName = "Extend.Portable.Testing" }
 )
 
 # Get all projects were ShouldBuild is true
