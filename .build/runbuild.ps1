cb0d484a84a1a1a197779ef0ae482535d02028d3
Dave Senn <dave.jon.senn@gmail.com>
2015-05-26 11:25:47 +0200

    Improve build

--- .build/runbuild.ps1
+++ .build/runbuild.ps1
@@ -3,28 +3,26 @@
     [hashtable] $properties = @{}
 )
 
-$path = Split-Path -Path $MyInvocation.MyCommand.Path
+$currentDirectory = Split-Path -Path $MyInvocation.MyCommand.Path
+$psakePath = [System.IO.Path]::Combine($currentDirectory, "..\", ".Tools\PSake\psake.psm1")
 
-write-host "Importing psake"
-Import-Module ($path + '\..\Tools\PSake\psake.psm1')
-
+Import-Module ($psakePath)
 Try
 {
-  write-host "Starting build.ps1"
-  Invoke-psake ($path + '\Build.ps1') -properties $properties -parameters $parameters
+    Invoke-Psake ($currentDirectory + '\Build.ps1') -properties $properties -parameters $parameters
 
-  if ($psake.build_success -eq $false)
-  {
-    write-host "build.ps1 failed" -fore RED
-    exit 1
-  }
-  else
-  {
-    write-host "build.ps1 succeeded" -fore GREEN
-    exit 0
-  }
+    if ($psake.build_success -eq $false)
+    {
+        Write-Host "Build failed" -fore Red
+        exit 1
+    }
+    else
+    {
+        Write-Host "Build succeeded" -fore Green
+        exit 0
+    }
 }
 Finally
 {
-  Remove-Module psake
+    Remove-Module psake
 }
