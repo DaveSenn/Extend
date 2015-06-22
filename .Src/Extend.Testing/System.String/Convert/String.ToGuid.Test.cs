b69d57c6bd288851338e85ffec2ba0c011af4e19
Dave Senn <dave.jon.senn@gmail.com>
2015-06-22 18:11:28 +0200

    Add unit tests for portable

---
+++ .Src/Extend.Testing/System.String/Convert/String.ToGuid.Test.cs
@@ -0,0 +1,30 @@
+﻿#region Usings
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void ToGuidTestCase()
+        {
+            var value = Guid.NewGuid();
+            var actual = value.ToString()
+                              .ToGuid();
+
+            Assert.AreEqual( value, actual );
+        }
+
+        [Test]
+        [ExpectedException( typeof (ArgumentNullException) )]
+        public void ToGuidTestCase1NullCheck()
+        {
+            StringEx.ToGuid( null );
+        }
+    }
+}
---
+++ .Src/Extend.Testing/System.String/ConvertSave/String.SaveToGuid.cs
@@ -0,0 +1,57 @@
+﻿#region Usings
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void SaveToGuidTestCase()
+        {
+            var expected = Guid.NewGuid();
+            var actual = expected.ToString()
+                                 .SaveToGuid();
+
+            Assert.AreEqual( expected, actual );
+        }
+
+        [Test]
+        public void SaveToGuidTestCase1()
+        {
+            var expected = Guid.NewGuid();
+            var actual = "InvalidValue".SaveToGuid( expected );
+
+            Assert.AreEqual( expected, actual );
+        }
+
+        [Test]
+        public void SaveToGuidTestCase4()
+        {
+            var expected = Guid.NewGuid();
+            var actual = expected.ToString()
+                                 .SaveToGuid( Guid.NewGuid() );
+
+            Assert.AreEqual( expected, actual );
+        }
+
+        [Test]
+        public void SaveToGuidTestCase5()
+        {
+            var actual = "InvalidValue".SaveToGuid();
+
+            Assert.AreEqual( default( Guid ), actual );
+        }
+
+        [Test]
+        [ExpectedException( typeof (ArgumentNullException) )]
+        public void SaveToGuidTestCaseNullCheck()
+        {
+            StringEx.SaveToGuid( null );
+        }
+    }
+}
