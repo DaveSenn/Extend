c2cd7132c79f91b95c750addecc8adeff685f9e3
Dave Senn <dave.jon.senn@gmail.com>
2015-06-22 17:50:41 +0200

    Add GUID extensions to portable

---
+++ .Src/Extend/System.String/Convert/String.ToGuid.cs
@@ -0,0 +1,25 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Extend
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        ///     Converts the given string to a GUID.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The value can not be null.</exception>
+        /// <exception cref="FormatException">value is not in a recognized format.</exception>
+        /// <param name="value">The string to convert.</param>
+        /// <returns>Returns the converted GUID.</returns>
+        public static Guid ToGuid( this String value )
+        {
+            value.ThrowIfNull( () => value );
+
+            return Guid.Parse( value );
+        }
+    }
+}
---
+++ .Src/Extend/System.String/ConvertSave/String.SaveToGuid.cs
@@ -0,0 +1,29 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Extend
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        ///     Converts the given string to a GUID.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The value can not be null.</exception>
+        /// <param name="value">The string to convert.</param>
+        /// <param name="defaultValue">
+        ///     The default value, returned if the parsing fails. If not provided default will be
+        ///     <see cref="Guid.Empty" />.
+        /// </param>
+        /// <returns>Returns the converted GUID.</returns>
+        public static Guid SaveToGuid( this String value, Guid? defaultValue = null )
+        {
+            value.ThrowIfNull( () => value );
+
+            Guid outValue;
+            return value.TryParsGuid( out outValue ) ? outValue : ( defaultValue ?? outValue );
+        }
+    }
+}
