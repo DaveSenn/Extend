615a4f142ff798474ebf57c416fb79e7d95dfced
Dave Senn <dave.jon.senn@gmail.com>
2015-09-27 15:59:13 +0200

    Add String.Contains extension

---
+++ .Src/Extend.Testing/System.String/String.Contains.Test.cs
@@ -0,0 +1,62 @@
+﻿#region Usings
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void ContainsTest()
+        {
+            const String target = "this is my string";
+            var actual = target.Contains( "string", StringComparison.OrdinalIgnoreCase );
+
+            actual.Should()
+                  .BeTrue();
+        }
+
+        [Test]
+        public void ContainsTest1()
+        {
+            const String target = "this is my string";
+            var actual = target.Contains( "String", StringComparison.Ordinal );
+
+            actual.Should()
+                  .BeFalse();
+        }
+
+        [Test]
+        public void ContainsTest2()
+        {
+            const String target = "this is my String";
+            var actual = target.Contains( "String", StringComparison.Ordinal );
+
+            actual.Should()
+                  .BeTrue();
+        }
+
+        [Test]
+        public void ContainsTestArgumentNullException()
+        {
+            const String target = null;
+            Action test = () => target.Contains( "String", StringComparison.Ordinal );
+
+            test.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ContainsTestArgumentNullException1()
+        {
+            const String target = "my string";
+            Action test = () => target.Contains( null, StringComparison.Ordinal );
+
+            test.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
