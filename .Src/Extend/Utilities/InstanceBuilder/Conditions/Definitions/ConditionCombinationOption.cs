e695fe0bc9854b969f6a26db26d75a06bc33d3d9
Dave Senn <dave.jon.senn@gmail.com>
2015-09-28 06:44:40 +0200

    Working on IB

---
+++ .Src/Extend/Utilities/InstanceBuilder/Conditions/Definitions/ConditionCombinationOption.cs
@@ -0,0 +1,23 @@
+﻿namespace Extend
+{
+    /// <summary>
+    ///     Enumeration of all condition combinations.
+    /// </summary>
+    public enum ConditionCombinationOption
+    {
+        /// <summary>
+        ///     Match all conditions.
+        /// </summary>
+        MatchAll = 0,
+
+        /// <summary>
+        ///     Match any condition.
+        /// </summary>
+        MatchAny = 1,
+
+        /// <summary>
+        ///     Don't match any condition.
+        /// </summary>
+        NotMatchAny = 3
+    }
+}
---
+++ .Src/Extend/Utilities/InstanceBuilder/Conditions/InstanceBuilderConditionCollection.cs
@@ -0,0 +1,46 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Collection of conditions.
+    /// </summary>
+    public class InstanceBuilderConditionCollection : List<IInstanceBuilderCondition>, IInstanceBuilderConditionCollection
+    {
+        #region Implementation of IInstanceBuilderConditionCollection
+
+        /// <summary>
+        ///     Gets or sets the combination option.
+        /// </summary>
+        /// <value>The combination option.</value>
+        public ConditionCombinationOption CombinationOption { get; set; }
+
+        /// <summary>
+        ///     Determines wheter the given arguments matches the conditions or not.
+        /// </summary>
+        /// <param name="arguments">The arguments to check.</param>
+        /// <returns>Returns a value of true if the arguments matches the conditions; oterwise, false.</returns>
+        public Boolean Matches( IInstanceValueArguments arguments )
+        {
+            switch ( CombinationOption )
+            {
+                case ConditionCombinationOption.MatchAll:
+                    return this.Any() && this.All( x => x.Matches( arguments ) );
+                case ConditionCombinationOption.MatchAny:
+                    return this.Any( x => x.Matches( arguments ) );
+                case ConditionCombinationOption.NotMatchAny:
+                    return this.Any() && this.All( x => !x.Matches( arguments ) );
+                default:
+                    throw new ArgumentOutOfRangeException( nameof( arguments ), arguments, $"Value '{arguments}' is not supported." );
+            }
+        }
+
+        #endregion
+    }
+}
