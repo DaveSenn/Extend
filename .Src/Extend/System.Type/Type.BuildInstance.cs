c8c8952798f3775d5a86b6782ae5a3acf1ebe227
Dave Senn <dave.jon.senn@gmail.com>
2015-09-28 10:47:35 +0200

    Working on IB

---
+++ .Src/Extend/System.Type/Type.BuildInstance.cs
@@ -0,0 +1,39 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="Type" />.
+    /// </summary>
+    public static partial class TypeEx
+    {
+        /// <summary>
+        ///     Builds an instance of the given type.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to create.</typeparam>
+        /// <returns>Returns the new created type.</returns>
+        public static T BuildInstance<T>() where T : class
+        {
+            return typeof (T).CreateInstanceBuilder()
+                             .Build<T>();
+        }
+
+        /// <summary>
+        ///     Builds an instance of the given type.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">type can not be null.</exception>
+        /// <param name="type">The type of the instance to create.</param>
+        /// <returns>Returns the new created type.</returns>
+        public static Object BuildInstance( this Type type )
+        {
+            type.ThrowIfNull( nameof( type ) );
+
+            return type.CreateInstanceBuilder()
+                       .Build();
+        }
+    }
+}
--- .Src/Extend/Utilities/InstanceBuilder/InstanceBuilder/InstanceBuilderWithCondition.cs
+++ .Src/Extend/Utilities/InstanceBuilder/InstanceBuilder/InstanceBuilderWithCondition.cs
@@ -14,7 +14,7 @@ namespace Extend
         ///     Gets the internal instance builder.
         /// </summary>
         /// <value>The internal instance builder.</value>
-        private IInstanceBuilder InstanceBuilder { get; }
+        private IIntegralInstanceBuilder InstanceBuilder { get; }
 
         /// <summary>
         ///     Gets the current factory.
@@ -31,7 +31,7 @@ namespace Extend
         /// </summary>
         /// <param name="instanceBuilder">The instance builder.</param>
         /// <param name="factory">The factory to crete conditions for.</param>
-        public InstanceBuilderWithCondition(IInstanceBuilder instanceBuilder, IInstanceValueFactory factory)
+        public InstanceBuilderWithCondition(IIntegralInstanceBuilder instanceBuilder, IInstanceValueFactory factory)
         {
             InstanceBuilder = instanceBuilder;
             Factory = factory;
