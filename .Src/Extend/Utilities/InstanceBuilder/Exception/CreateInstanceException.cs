872f7582815a988d3b33cec25174f0222e5e1c98
Dave Senn <dave.jon.senn@gmail.com>
2015-10-31 15:11:19 +0100

    Cleanup old instance builder

---
+++ .Src/Extend/Utilities/InstanceBuilder/Exception/CreateInstanceException.cs
@@ -0,0 +1,79 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Exception thrown when the creation of an instance fails.
+    /// </summary>
+    public class CreateInstanceException : Exception
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets information about the factories.
+        /// </summary>
+        /// <value>Information about the factories.</value>
+        public String FactoryInformation { get; }
+
+        /// <summary>
+        ///     Gets information about the selection.
+        /// </summary>
+        /// <value>Information about the selection rules.</value>
+        public String SelectionRuleRuleInformation { get; }
+
+        /// <summary>
+        ///     Gets information about the member.
+        /// </summary>
+        /// <value>Information about the member.</value>
+        public IMemberInformation MemberInformation { get; }
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CreateInstanceException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public CreateInstanceException( String message )
+            : base( message )
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CreateInstanceException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference  if no inner exception is specified.
+        /// </param>
+        public CreateInstanceException( String message, Exception innerException )
+            : base( message, innerException )
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CreateInstanceException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference  if no inner exception is specified.
+        /// </param>
+        /// <param name="factoryInformation">Information about the factories.</param>
+        /// <param name="selectionRuleInformation">Information about selection rules.</param>
+        /// <param name="memberInformation">The current member.</param>
+        public CreateInstanceException( String message, Exception innerException, String factoryInformation, String selectionRuleInformation, IMemberInformation memberInformation )
+            : base( message, innerException )
+        {
+            FactoryInformation = factoryInformation;
+            SelectionRuleRuleInformation = selectionRuleInformation;
+            MemberInformation = memberInformation;
+        }
+
+        #endregion
+    }
+}
---
+++ .Src/Extend/Utilities/InstanceBuilder/MemberInformation/Definition/IMemberInformation.cs
@@ -0,0 +1,55 @@
+﻿#region Usings
+
+using System;
+using System.Reflection;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Interface exposing information about a member.
+    /// </summary>
+    public interface IMemberInformation
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets the name of the member.
+        /// </summary>
+        /// <value>The name of the member.</value>
+        String MemberName { get; }
+
+        /// <summary>
+        ///     Gets the member path of the member.
+        /// </summary>
+        /// <value>The member path of the member.</value>
+        String MemberPath { get; }
+
+        /// <summary>
+        ///     Gets the type of the member.
+        /// </summary>
+        /// <value>The type of the member.</value>
+        Type MemberType { get; }
+
+        /// <summary>
+        ///     Gets or sets a reference to the member.
+        /// </summary>
+        /// <remarks>
+        ///     Not always set.
+        /// </remarks>
+        /// <value>A reference to the member.</value>
+        Object MemberObject { get; }
+
+        /// <summary>
+        ///     Gets the property info representing the member.
+        /// </summary>
+        /// <remarks>
+        ///     Not always set.
+        /// </remarks>
+        /// <value>The property info representing the member.</value>
+        PropertyInfo PropertyInfo { get; }
+
+        #endregion
+    }
+}
