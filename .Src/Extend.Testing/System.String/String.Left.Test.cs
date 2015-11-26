742e8c0298c3f27de59ad8e0c899e756174be496
Dave Senn <dave.jon.senn@gmail.com>
2015-11-26 18:27:13 +0100

    Add Left

---
+++ .Src/Extend.Testing/System.String/String.Left.Test.cs
@@ -0,0 +1,57 @@
+﻿#region Usings
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend.Testing
+{
+    [TestFixture]
+    public partial class StringExTest
+    {
+        [Test]
+        public void LeftArgumentNullExceptionTest()
+        {
+            String value = null;
+            Action test = () => value.Left( 1 );
+
+            test.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void LeftArgumentOutOfRangeExceptionTest()
+        {
+            Action test = () => "test".Left( -1 );
+
+            test.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void LeftArgumentOutOfRangeExceptionTest1()
+        {
+            Action test = () => "test".Left( 10 );
+
+            test.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void LeftTest()
+        {
+            var actual = "This is a test".Left( 6 );
+
+            actual.Should()
+                  .Be( "This i" );
+        }
+
+        [Test]
+        public void LeftTest1()
+        {
+            var actual = "".Left( 0 );
+
+            actual.Should()
+                  .Be( String.Empty );
+        }
+    }
+}
