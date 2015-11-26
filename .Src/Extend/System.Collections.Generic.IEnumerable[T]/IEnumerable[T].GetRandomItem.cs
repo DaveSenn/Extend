d8e395b988c086232ee157a624acd99ab64d155f
Dave Senn <dave.jon.senn@gmail.com>
2015-11-26 09:20:29 +0100

    Add new extension methods

---
+++ .Src/Extend/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].GetRandomItem.cs
@@ -0,0 +1,34 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="IEnumerable{T}" />.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static partial class IEnumerableTEx
+    {
+        /// <summary>
+        ///     Gets a random item form the given IEnumerable.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The enumerable can not be null.</exception>
+        /// <typeparam name="T">The type of the items in the enumerable.</typeparam>
+        /// <param name="enumerable">The IEnumerable.</param>
+        /// <returns>Returns an random item of the given IEnumerable.</returns>
+        public static T GetRandomItem<T>( this IEnumerable<T> enumerable )
+        {
+            enumerable.ThrowIfNull( nameof( enumerable ) );
+
+            var array = enumerable as T[] ?? enumerable.ToArray();
+
+            var index = RandomValueEx.GetRandomInt32( 0, array.Count() );
+            return array.ElementAt( index );
+        }
+    }
+}
