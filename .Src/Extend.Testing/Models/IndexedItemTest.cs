da76c7787d10c9db90b3eb9cc1ec09a1b342b1c9
Dave Senn <dave.jon.senn@gmail.com>
2015-11-30 08:01:24 +0100

    Add new extensions Sign assembly

---
+++ .Src/Extend.Testing/Models/IndexedItemTest.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend.Testing
+{
+    [TestFixture]
+    public class IndexedItemTest
+    {
+        [Test]
+        public void CtorTest()
+        {
+            var index = RandomValueEx.GetRandomInt32();
+            var value = RandomValueEx.GetRandomString();
+            var target = new IndexedItem<String>( index, value );
+
+            target.Index.Should()
+                  .Be( index );
+            target.Item.Should()
+                  .Be( value );
+        }
+    }
+}
---
+++ .Src/Extend.Testing/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].Append.Test.cs
@@ -0,0 +1,40 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Extend.Testing
+{
+    [TestFixture]
+    public partial class IEnumerableTExTest
+    {
+        [Test]
+        public void AppendArgumentNullExceptionTest()
+        {
+            List<String> list = null;
+            Action test = () => list.Append( "d" );
+
+            test.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void AppendTest()
+        {
+            var list = new List<String> { "a", "b", "c" };
+            var actual = list.Append( "d" );
+
+            list.Should()
+                .HaveCount( 3 );
+            actual.Should()
+                  .Contain( "d" );
+            actual.ElementAt( 3 )
+                  .Should()
+                  .Be( "d" );
+        }
+    }
+}
