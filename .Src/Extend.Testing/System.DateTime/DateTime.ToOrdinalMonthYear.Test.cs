03e4662a115bf90fedfef48812c059583c9b1de6
ryanhry <ryan.hryczynski@gmail.com>
2018-10-29 12:48:13 -0700

    add datetime toordinal format extension method

---
+++ .Src/Extend.Testing/System.DateTime/DateTime.ToOrdinalMonthYear.Test.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+
+using System;
+using Xunit;
+
+#endregion
+
+namespace Extend.Testing
+{
+    public partial class DateTimeExTest
+    {
+        [Fact]
+        public void ToOrdinalMonthYearTest()
+        {
+            var date = new DateTime(2011, 2, 11);
+            Assert.Equal("February 11th, 2011", date.ToOrdinalMonthYear());
+            date = new DateTime(2011, 2, 2);
+            Assert.Equal("February 2nd, 2011", date.ToOrdinalMonthYear());
+            date = new DateTime(2011, 2, 1);
+            Assert.Equal("February 1st, 2011", date.ToOrdinalMonthYear());
+            date = new DateTime(2011, 2, 23);
+            Assert.Equal("February 23rd, 2011", date.ToOrdinalMonthYear());
+            date = new DateTime(2011, 2, 24);
+            Assert.Equal("February 24th, 2011", date.ToOrdinalMonthYear());
+        }
+    }
+}
---
+++ .Src/Extend/System.DateTime/DateTime.ToOrdinalMonthYear.cs
@@ -0,0 +1,48 @@
+﻿#region Usings
+
+using System;
+using System.Linq;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Extend
+{
+    /// <summary>
+    ///     Class containing some extension methods for <see cref="DateTime" />.
+    /// </summary>
+    public static partial class DateTimeEx
+    {
+
+        /// <summary>
+        /// Returns the date in year month ordinal format
+        /// </summary>
+        [Pure]
+        [PublicAPI]
+		public static string ToOrdinalMonthYear(this DateTime date)
+        {
+            string suffix;
+            if (new[] { 11, 12, 13 }.Contains(date.Day))
+            {
+                suffix = "th";
+            }
+            else if (date.Day % 10 == 1)
+            {
+                suffix = "st";
+            }
+            else if (date.Day % 10 == 2)
+            {
+                suffix = "nd";
+            }
+            else if (date.Day % 10 == 3)
+            {
+                suffix = "rd";
+            }
+            else
+            {
+                suffix = "th";
+            }
+            return string.Format("{0:MMMM} {1}{2}, {0:yyyy}", date, date.Day, suffix);
+        }
+    }
+}
